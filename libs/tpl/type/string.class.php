67f7a10defec8b2b5b5a8691f609ba655929c519
Harald Lapp <harald@octris.org>
2010-06-26 15:47:44 +0200

    - initial commit template engine classes, just started work on template engine

---
+++ libs/tpl/type/string.class.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace org\octris\core\tpl\type {
+    /****c* type/string
+     * NAME
+     *      string
+     * FUNCTION
+     *      string type of template class
+     * COPYRIGHT
+     *      copyright (c) 2010 by Harald Lapp
+     * AUTHOR
+     *      Harald Lapp <harald@octris.org>
+     ****
+     */
+
+    class string extends \org\octris\core\type\string {
+        /****m* string/default
+         * SYNOPSIS
+         */
+        function default()
+        /*
+         * FUNCTION
+         *      
+         * INPUTS
+         *      
+         * OUTPUTS
+         *      
+         ****
+         */
+        {
+            
+        }
+    }
+}
