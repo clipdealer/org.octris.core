bc9a87f2648f9879744c56b402602397ae9eeafc
Harald Lapp <harald@octris.org>
2010-06-26 17:07:36 +0200

    - enable parameter wrapper for the super-globals _SERVER and _ENV in cli applications

--- libs/app/cli.class.php
+++ libs/app/cli.class.php
@@ -3,5 +3,9 @@
 namespace org\octris\core\app {
     class cli extends \org\octris\core\app {
     }
+
+    // enable validation for superglobals
+    $_SERVER  = new wrapper($_SERVER);
+    $_ENV     = new wrapper($_ENV);
 }
 
