29183a30d6210ae6d1541c4a6d6701dcfe8e5049
Harald Lapp <harald@air.(none)>
2010-06-26 15:47:44 +0200

    - initial commit template engine classes, just started work on template engine

---
+++ libs/tpl.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace org\octris\core {
+    /****c* core/tpl
+     * NAME
+     *      tpl
+     * FUNCTION
+     *      template engine main class
+     * COPYRIGHT
+     *      copyright (c) 2010 by Harald Lapp
+     * AUTHOR
+     *      Harald Lapp <harald@octris.org>
+     ****
+     */
+
+    class tpl {
+        
+    }
+}
---
+++ libs/tpl/type.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace org\octris\core\tpl {
+    /****c* tpl/type
+     * NAME
+     *      type
+     * FUNCTION
+     *      core type of template engine
+     * COPYRIGHT
+     *      copyright (c) 2010 by Harald Lapp
+     * AUTHOR
+     *      Harald Lapp <harald@octris.org>
+     ****
+     */
+
+    class type {
+        
+    }
+}
