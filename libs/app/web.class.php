efb439521c645679a007541269745f1dbc29e8e6
Harald Lapp <harald@octris.org>
2010-06-26 17:08:08 +0200

    - enable parameter wrapper for the super-globals _COOKIE, _GET, _POST, _REQUEST, _SERVER and _ENV in web applications

--- libs/app/web.class.php
+++ libs/app/web.class.php
@@ -3,5 +3,13 @@
 namespace org\octris\core\app {
     class web extends \org\octris\core\app {
     }
+
+    // enable validation for superglobals
+    $_COOKIE  = new wrapper($_COOKIE);
+    $_GET     = new wrapper($_GET);
+    $_POST    = new wrapper($_POST);
+    $_SERVER  = new wrapper($_SERVER);
+    $_ENV     = new wrapper($_ENV);
+    $_REQUEST = new wrapper($_REQUEST);
 }
 
