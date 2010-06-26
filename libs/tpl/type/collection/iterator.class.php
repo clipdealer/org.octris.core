ce521642c973002b71519ec1afc06649c6eab2fb
Harald Lapp <harald@air.(none)>
2010-06-26 17:41:50 +0200

    - implemented collection iterator, which is not longer derived from ArrayIterator, because the ArrayIterator class provides way to much functionality, we do not need in the template engine

--- libs/tpl/type/collection/iterator.class.php
+++ libs/tpl/type/collection/iterator.class.php
@@ -13,29 +13,173 @@ namespace org\octris\core\tpl\type\collection {
      ****
      */
 
-    class iterator extends \ArrayIterator {
+    class iterator implements \Iterator, \SeekableIterator, \Countable {
+        /****v* iterator/$position
+         * SYNOPSIS
+         */
+        protected $position = 0;
+        /*
+         * FUNCTION
+         *      iterator position
+         ****
+         */
+        
+        /****v* iterator/$keys
+         * SYNOPSIS
+         */
+        protected $keys = array();
+        /*
+         * FUNCTION
+         *      keys of data
+         ****
+         */
+        
+        /****v* iterator/$data
+         * SYNOPSIS
+         */
+        protected $data = array();
+        /*
+         * FUNCTION
+         *      iterator data
+         ****
+         */
+        
+        /****v* iterator/$count
+         * SYNOPSIS
+         */
+        protected $count = 0;
+        /*
+         * FUNCTION
+         *      number of items
+         ****
+         */
+        
+        /****m* iterator/__construct
+         * SYNOPSIS
+         */
+        function __construct($data)
+        /*
+         * FUNCTION
+         *      constructor
+         * INPUTS
+         *      * $data (mixed) -- data for iterating over
+         ****
+         */
+        {
+            $this->position = 0;
+            $this->keys     = array_keys($data);
+            $this->data     = $data;
+            $this->count    = count($this->keys);
+        }
+        
+        /****m* iterator/current
+         * SYNOPSIS
+         */
+        function current()
+        /*
+         * FUNCTION
+         *      return current array entry
+         * OUTPUTS
+         *      (mixed) -- value of current entry
+         ****
+         */
+        {
+            return $this->data[$this->keys[$this->position]];
+        }
+        
+        /****m* iterator/key
+         * SYNOPSIS
+         */
+        function key()
+        /*
+         * FUNCTION
+         *      return key of current array entry
+         * OUTPUTS
+         *      (string) -- key of current array entry
+         ****
+         */
+        {
+            return $this->keys[$this->position];
+        }
+        
+        /****m* iterator/next
+         * SYNOPSIS
+         */
+        function next()
+        /*
+         * FUNCTION
+         *      move to next entry
+         ****
+         */
+        {
+            ++$this->position;
+        }
+        
+        /****m* iterator/rewind
+         * SYNOPSIS
+         */
+        function rewind()
+        /*
+         * FUNCTION
+         *      rewind array back to the start
+         ****
+         */
+        {
+            $this->position = 0;
+        }
+        
+        /****m* iterator/valid
+         * SYNOPSIS
+         */
+        function valid()
+        /*
+         * FUNCTION
+         *      test if the current position is valid
+         * OUTPUTS
+         *      (bool) -- returns true for valid positions
+         ****
+         */
+        {
+            return ($this->position < $this->count);
+        }
+        
+        /****m* iterator/seek
+         * SYNOPSIS
+         */
+        function seek($position)
+        /*
+         * FUNCTION
+         *      seek to position
+         * INPUTS
+         *      * $position (int) -- position to seek to
+         ****
+         */
+        {
+            $this->position = $position;
+        }
+        
         /****m* iterator/getItem
          * SYNOPSIS
          */
         protected function getItem($pos)
         /*
          * FUNCTION
-         *      get item in current position
+         *      return item for specified position
          * INPUTS
          *      * $pos (int) -- position to return item for
          * OUTPUTS
-         *      (mixed) item value
+         *      (mixed) -- item value
          ****
          */
         {
-            $item = $this->values[$pos];
-        
+            $item = $this->data[$this->keys[$pos]];
+            
             switch (gettype($item)) {
-            case 'array':
-                $item = new self($item);
-                break;
             case 'object':
-                $item = new self((array)$item);
+                $item = (array)$item;
+                /** FALL THRU **/
+            case 'array':
+                $item = new \org\octris\core\tpl\type\collection($item);
                 break;
             }
         
