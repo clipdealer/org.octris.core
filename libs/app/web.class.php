472dafd5d32a0a4de5a5e155828f95fe95e7dd48
Harald Lapp <harald@air.(none)>
2010-06-26 21:26:10 +0200

    - fixed namespace

--- libs/app/web.class.php
+++ libs/app/web.class.php
@@ -5,11 +5,11 @@ namespace org\octris\core\app {
     }
 
     // enable validation for superglobals
-    $_COOKIE  = new wrapper($_COOKIE);
-    $_GET     = new wrapper($_GET);
-    $_POST    = new wrapper($_POST);
-    $_SERVER  = new wrapper($_SERVER);
-    $_ENV     = new wrapper($_ENV);
-    $_REQUEST = new wrapper($_REQUEST);
+    $_COOKIE  = new \org\octris\core\validate\wrapper($_COOKIE);
+    $_GET     = new \org\octris\core\validate\wrapper($_GET);
+    $_POST    = new \org\octris\core\validate\wrapper($_POST);
+    $_SERVER  = new \org\octris\core\validate\wrapper($_SERVER);
+    $_ENV     = new \org\octris\core\validate\wrapper($_ENV);
+    $_REQUEST = new \org\octris\core\validate\wrapper($_REQUEST);
 }
 
