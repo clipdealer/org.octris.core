12adab89aa81b40ed772ae13d50d9093667f05bd
Harald Lapp <harald@octris.org>
2010-06-27 18:42:08 +0200

    - modified validation test

--- tests/validate.php
+++ tests/validate.php
@@ -3,7 +3,7 @@
 $_POST = array('p1' => 1, 'p2' => 2);
 
 require_once('org.octris.core/app.class.php');
-require_once('org.octris.core/validate/wrapper.class.php');
+require_once('org.octris.core/app/web.class.php');
 
 $v = new \org\octris\core\validate\schema(array(
     'default' => array(                        // entry point, always required!
@@ -18,14 +18,14 @@ $v = new \org\octris\core\validate\schema(array(
         )
     )
 ), 'array');
-$r = (int)$v->validate($_POST);
+// $r = (int)$v->validate($_POST);
+
+print "p1->isSet: " . (int)$_POST['p1']->isSet . "\n";
+print "p1->validate: " . (int)$_POST['p1']->validate('digit') . "\n";
 
 foreach ($_POST as $k => $v) {
     print "$k";
     print_r($v);
 }
-
-print (int)($_POST instanceof Traversable);
-// print $r;
 
 ?>
