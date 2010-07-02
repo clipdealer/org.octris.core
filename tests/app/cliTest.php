78bac905cc4d5a01ef36959b20be219b3c8674e5
Harald Lapp <harald@air.(none)>
2010-07-02 20:23:53 +0200

    - initial commit test for cli / getOptions

---
+++ tests/app/cliTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('org.octris.core/app/test.class.php');
+
+use \org\octris\core\app\test as test;
+
+class cliTest extends PHPUnit_Framework_TestCase {
+    public function testGetOptions() {
+        $GLOBALS['argv'] = array(
+            'test', '--option1', '--option2=test', '--option3=test test',
+            '-o', '-p', 'option5', '-q', 'option6 test', '-abc',
+            '--option8', 'test', '--option9', 'with whitespace'
+        );
+
+        $options = \org\octris\core\app\cli::getOptions();
+
+        $this->assertEquals(array(
+          'option1' => true, 'option2' => 'test', 'option3' => 'test test',
+          'o' => true, 'p' => 'option5', 'q' => 'option6 test',
+          'a' => true, 'b' => true, 'c' => true,
+          'option8' => 'test', 'option9' => 'with whitespace',
+        ), $options);
+    }
+}
