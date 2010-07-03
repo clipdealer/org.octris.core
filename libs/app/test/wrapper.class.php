3a91fd1b2c5ea0b49c38ecad7cf79b57ae0542c5
Harald Lapp <harald@air.(none)>
2010-07-03 17:56:10 +0200

    - implemented special wrapper for handling PHPUnit

---
+++ libs/app/test/wrapper.class.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace org\octris\core\app\test {
+    /****c* test/wrapper
+     * NAME
+     *      wrapper
+     * FUNCTION
+     *      This wrapper is required for PHPUnit, because PHPUnit accesses
+     *      the PHP superglobales in the PHP way, but the OCTRiS framework
+     *      might have overwritten the superglobals by it's own implementation.
+     *      The Wrapper wraps the OCTRiS frameworks functionality and checks,
+     *      if access is requested from PHPUnit or from an OCTRiS framework
+     *      component. Access is granted the PHP way for PHPUnit requests,
+     *      for all other requests, it will expose the OCTRiS frameworks way.
+     * COPYRIGHT
+     *      copyright (c) 2010 by Harald Lapp
+     * AUTHOR
+     *      Harald Lapp <harald@octris.org>
+     ****
+     */
+
+    class wrapper extends \org\octris\core\validate\wrapper {
+        /****m* wrapper/__construct
+         * SYNOPSIS
+         */
+        function __construct($source)
+        /*
+         * FUNCTION
+         *      constructor
+         * INPUTS
+         *      * $source (wrapper) -- source wrapper object
+         ****
+         */
+        {
+            $this->data = $source->getArrayCopy();
+            $this->keys = array_keys($this->data);
+        }
+        
+        /****m* wrapper/offsetGet
+         * SYNOPSIS
+         */
+        function offsetGet($offs)
+        /*
+         * FUNCTION
+         *      return array entry of specified offset
+         * INPUTS
+         *      * $offs (string) -- offset
+         * OUTPUTS
+         *      (stdClass) -- value
+         ****
+         */
+        {
+            $trace = debug_backtrace();
+            
+            if (strpos($trace[0]['file'], '/PHPUnit/') !== false) {
+                // call from PHPUnit
+                return $this->data[$offs]->tainted;
+            } else {
+                return parent::offsetGet($offs);
+            }
+        }
+    }
+}
