56c53664c74a8e1e70f99e5cab8d98d6f5a5f4aa
Harald Lapp <harald@air.(none)>
2010-07-03 21:42:43 +0200

    - started work on test cases for config class

---
+++ tests/configTest.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('org.octris.core/app/test.class.php');
+
+use \org\octris\core\app\test as test;
+
+class configTest extends PHPUnit_Framework_TestCase {
+    protected function loadConfig() {
+        
+    }
+    
+    public function testHasStaticDataProperty() {
+        $class = new ReflectionClass('\org\octris\core\config');
+        $this->assertTrue($class->hasProperty('data'));
+        
+        $prop = $class->getProperty('data');
+        $this->assertTrue($prop->isStatic());
+    }
+    
+    public function 
+}
