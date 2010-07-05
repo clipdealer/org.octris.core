737565805fa6d6ce3eb9c39d0c66becc7f3559b9
Harald Lapp <harald@octris.org>
2010-07-05 09:23:12 +0200

    - added collection methods for: flatten, deflatten, utf8Encode and utf8Decode

--- libs/type/collection.class.php
+++ libs/type/collection.class.php
@@ -218,6 +218,143 @@ namespace org\octris\core\type {
         {
             return count($this->data);
         }
+        
+        /****m* collection/flatten
+         * SYNOPSIS
+         */
+        function flatten($sep = '.') 
+        /*
+         * FUNCTION
+         *      flatten an array. convert a recursive index or key/value based array into a flat array with expanded
+         *      keys.
+         * INPUTS
+         *      * $sep (string) -- (optional) separator for expanding keys
+         * OUTPUTS
+         *      (collection) -- flattened array
+         ****
+         */
+        {
+            $tmp = array();
+
+            $array = new RecursiveIteratorIterator(new RecursiveArrayIterator($this->data), RecursiveIteratorIterator::SELF_FIRST);
+            $d = 0;
+
+            $property = array();
+
+            foreach ($array as $k => $v) {
+                if (!is_int($k)) {
+                    if ($d > $array->getDepth()) {
+                        array_splice($property, $array->getDepth());
+                    }
+
+                    $property[$array->getDepth()] = $k;
+
+                    $d = $array->getDepth();
+                }
+
+                if (is_int($k)) {
+                    $tmp[implode($sep, $property)][] = $v;
+                } elseif (!is_array($v)) {
+                    $tmp[implode($sep, $property)] = $v;
+                }
+            }
+
+            return new collection($tmp);
+        }
+
+        /****m* collection/deflatten
+         * SYNOPSIS
+         */
+        function deflatten()
+        /*
+         * FUNCTION
+         *      deflatten an array, which was flattened with flatten method
+         ****
+         */
+        {
+            $tmp = array();
+
+            foreach ($this->data as $k => $v) {
+                $key  = explode('.', $k);
+                $ref =& $tmp;
+
+                foreach ($key as $part) {
+                    if (!isset($ref[$part])) {
+                        $ref[$part] = array();
+                    }
+
+                    $ref =& $ref[$part];
+                }
+
+                $ref = $v;
+            }
+
+            return new collection($tmp);
+        }
+        
+        /****m* collection/utf8Encode
+         * SYNOPSIS
+         */
+        function utf8Encode()
+        /*
+         * FUNCTION
+         *      utf8 encode collection values
+         * OUTPUTS
+         *      (collection) -- encoded data
+         ****
+         */
+        {
+            $tmp = $this->data;
+            
+            array_walk_recursive($tmp, function(&$v) {
+                if (is_string($v) && !preg_match('%^(?:  
+                [\x09\x0A\x0D\x20-\x7E] # ASCII  
+                | [\xC2-\xDF][\x80-\xBF] # non-overlong 2-byte  
+                | \xE0[\xA0-\xBF][\x80-\xBF] # excluding overlongs  
+                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte  
+                | \xED[\x80-\x9F][\x80-\xBF] # excluding surrogates  
+                | \xF0[\x90-\xBF][\x80-\xBF]{2} # planes 1-3  
+                | [\xF1-\xF3][\x80-\xBF]{3} # planes 4-15  
+                | \xF4[\x80-\x8F][\x80-\xBF]{2} # plane 16  
+                )*$%xs', $v)) {
+                    $v = utf8_encode($v);
+                }            
+            });
+
+            return new collection($tmp);
+        }
+
+        /****m* collection/utf8Decode
+         * SYNOPSIS
+         */
+        function utf8Decode()
+        /*
+         * FUNCTION
+         *      utf8 decode collection
+         * OUTPUTS
+         *      (collection) -- decoded data
+         ****
+         */
+        {
+            $tmp = $this->data;
+            
+            array_walk_recursive($tmp, function(&$v) {
+                if (is_string($v) && preg_match('%^(?:  
+                [\x09\x0A\x0D\x20-\x7E] # ASCII  
+                | [\xC2-\xDF][\x80-\xBF] # non-overlong 2-byte  
+                | \xE0[\xA0-\xBF][\x80-\xBF] # excluding overlongs  
+                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte  
+                | \xED[\x80-\x9F][\x80-\xBF] # excluding surrogates  
+                | \xF0[\x90-\xBF][\x80-\xBF]{2} # planes 1-3  
+                | [\xF1-\xF3][\x80-\xBF]{3} # planes 4-15  
+                | \xF4[\x80-\x8F][\x80-\xBF]{2} # plane 16  
+                )*$%xs', $v)) {
+                    $v = utf8_decode($v);
+                }            
+            });
+
+            return new collection($tmp);
+        }
     }
 }
 
