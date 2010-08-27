5c84a1e37995fc8fc8be9775b31d3d73f2d64fcd
Harald Lapp <harald@octris.org>
2010-08-27 18:23:41 +0200

    - started work on a test

---
+++ tests/tpl/compiler/macroTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('org.octris.core/app/test.class.php');
+
+use \org\octris\core\app\test as test;
+
+class macroTest extends PHPUnit_Framework_TestCase {
+    public function testHasStaticDataProperty() {
+        $class = new ReflectionClass('\org\octris\core\config');
+        $this->assertTrue($class->hasProperty('data'));
+        
+        $prop = $class->getProperty('data');
+        $this->assertTrue($prop->isStatic());
+    }
+}
