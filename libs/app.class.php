470c5dfd63df338014e7134ef4299a9d9a54d1ec
Harald Lapp <harald@octris.org>
2010-08-27 18:22:40 +0200

    - work on app base class

--- libs/app.class.php
+++ libs/app.class.php
@@ -16,6 +16,19 @@ namespace org\octris\core {
      */
 
     abstract class app {
+        /****d* app/T_CONTEXT_UNDEFINED, T_CONTEXT_CLI, T_CONTEXT_WEB, T_CONTEXT_TEST
+         * SYNOPSIS
+         */
+        const T_CONTEXT_UNDEFINED = 0;
+        const T_CONTEXT_CLI       = 1;
+        const T_CONTEXT_WEB       = 2;
+        const T_CONTEXT_TEST      = 3;
+        /*
+         * FUNCTION
+         *      context the application is running in
+         ****
+         */
+        
         /****v* app/$instance
          * SYNOPSIS
          */
@@ -23,6 +36,16 @@ namespace org\octris\core {
         /*
          * FUNCTION
          *      application instance
+         ****
+         */
+        
+        /****v* app/$context
+         * SYNOPSIS
+         */
+        protected static $context = self::T_CONTEXT_UNDEFINED;
+        /*
+         * FUNCTION
+         *      application context
          ****
          */
         
@@ -82,6 +105,21 @@ namespace org\octris\core {
          */
         {
             // TODO: implementation
+        }
+        
+        /****m* app/getContext
+         * SYNOPSIS
+         */
+        public static final function getContext()
+        /*
+         * FUNCTION
+         *      Return context the application is running in.
+         * OUTPUTS
+         *      (int) -- application context
+         ****
+         */
+        {
+            return static::$context;
         }
         
         /****m* app/getInstance
