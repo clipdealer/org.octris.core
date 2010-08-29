7e4b43e42ed343b0775b15f6bd215d1dda0998db
Harald Lapp <harald@air.(none)>
2010-08-29 15:55:51 +0200

    - initial commit -- lib for rewriting template functions

---
+++ libs/tpl/compiler/rewrite.class.php
@@ -0,0 +1,294 @@
+<?php
+
+namespace org\octris\core\tpl\compiler {
+    /****c* compiler/rewrite
+     * NAME
+     *      rewrite
+     * FUNCTION
+     *      Rewrite template code. Rewrite inline function calls, and rewrite
+     *      function calls according to if they are allowed php function calls
+     *      or calls to functions that have to be registered to sandbox on
+     *      template rendering.
+     * COPYRIGHT
+     *      copyright (c) 2010 by Harald Lapp
+     * AUTHOR
+     *      Harald Lapp <harald@octris.org>
+     ****
+     */
+
+    class rewrite {
+        /****v* rewrite/$inline
+         * SYNOPSIS
+         */
+        protected static $inline = array(
+            '_'     => array('min' => 1),               // gettext
+            'if'    => array('min' => 2, 'max' => 3),   // (... ? ... : ...)
+            'mul'   => array('min' => 2),               // ... * ...
+            'div'   => array('min' => 2),               // ... / ...
+            'mod'   => array('min' => 2, 'max' => 2),   // ... % ...
+            'add'   => array('min' => 2),               // ... + ...
+            'sub'   => array('min' => 2),               // ... - ...
+            'incr'  => array('min' => 1, 'max' => 1),   // ++...
+            'decr'  => array('min' => 1, 'max' => 1),   // --...
+            'and'   => array('min' => 2),               // ... && ...
+            'or'    => array('min' => 2),               // ... || ...
+            'xor'   => array('min' => 2, 'max' => 2),   // ... xor ...
+            'not'   => array('min' => 1, 'max' => 1),   // !...
+            'lt'    => array('min' => 2, 'max' => 2),   // ... < ...
+            'gt'    => array('min' => 2, 'max' => 2),   // ... > ...
+            'eq'    => array('min' => 2, 'max' => 2),   // ... == ...
+            'le'    => array('min' => 2, 'max' => 2),   // ... <= ...
+            'ge'    => array('min' => 2, 'max' => 2),   // ... >= ...
+            'ne'    => array('min' => 2, 'max' => 2),   // ... != ...
+        );
+        /*
+         * FUNCTION
+         *      inline method rewrite
+         ****
+         */
+        
+        /****v* rewrite/$phpfunc
+         * SYNOPSIS
+         */
+        protected static $phpfunc = array(
+            'substr'    => array('min' => 2, 'max' => 3)
+        );
+        /*
+         * FUNCTION
+         *      allowed php functions
+         ****
+         */
+        
+        /****v* rewrite/$last_error
+         * SYNOPSIS
+         */
+        protected static $last_error = '';
+        /*
+         * FUNCTION
+         *      last error message
+         ****
+         */
+        
+        /****m* rewrite/__callStatic
+         * SYNOPSIS
+         */
+        public static function __callStatic($name, $args)
+        /*
+         * FUNCTION
+         *      wrapper for methods that can be optimized
+         * INPUTS
+         *      * $name (string) -- name of method
+         *      * $args (array) -- arguments for method
+         * OUTPUTS
+         *      
+         ****
+         */
+        {
+            self::$last_error = '';
+            
+            $args = $args[0];
+            
+            if (($is_php = isset(self::$phpfunc[$name])) || isset(self::$inline[$name])) {
+                // known method
+                $cnt = count($args);
+                
+                if (isset(self::$inline[$name]['min'])) {
+                    if ($cnt < self::$inline[$name]['min']) {
+                        self::setError($name, 'not enough arguments');
+                    }
+                }
+                if (isset(self::$inline[$name]['max'])) {
+                    if ($cnt > self::$inline[$name]['max']) {
+                        self::setError($name, 'to many arguments');
+                    }
+                }
+                
+                if ($is_php) {
+                    // call to allowed PHP function
+                    return $name . '(' . implode(', ', $args) . ')';
+                } else {
+                    // inline function rewrite
+                    $name = "_$name";
+
+                    return self::$name($args);
+                }
+            } else {
+                return sprintf(
+                    '$this->%s(%s)',
+                    $name,
+                    implode(', ', $args)
+                );
+            }
+        }
+        
+        /****m* rewrite/getError
+         * SYNOPSIS
+         */
+        public static function getError()
+        /*
+         * FUNCTION
+         *      return last occured error message
+         * OUTPUTS
+         *      (string) -- error message
+         ****
+         */
+        {
+            return self::$last_error;
+        }
+
+        /****m* rewrite/setError
+         * SYNOPSIS
+         */
+        protected static function setError($func, $msg)
+        /*
+         * FUNCTION
+         *      set an error message
+         * INPUTS
+         *      * $func (string) -- name of function the error occured for
+         *      * $msg (string) -- additional error message
+         ****
+         */
+        {
+            self::$last_error = sprintf('"%s" -- %s', $func, $msg);
+        }
+
+        /*
+         * next: inline functions, that can be converted directly
+         */
+        protected static function __($args) {
+            if (preg_match('/^(["\'])(.*)\1$/', $args[0], $match)) {
+                // string
+                $chr = $match[1];   // quotation character
+                $txt = $match[2];   // text to translate TODO: l10n->lookup
+                
+                $txt = $chr . addcslashes($txt, ($chr == '"' ? '"' : "'")) . $chr;
+                
+                array_shift($args);
+                
+                if (count($args) > 0) {
+                    $replace = array();
+                    $pattern = '/\[(?:(_\d+)|(?:([^,]+))(?:,(.*?))?(?<!\\\))\]/sie';
+
+                    if (preg_match_all($pattern, $txt, $match, PREG_SET_ORDER)) {
+                        foreach ($match as $m) {
+                            $str = $m[0];
+
+                            if (!isset($m[2])) {
+                                $cmd = '';
+                            } else {
+                                $cmd = $m[2];
+                                unset($m[2]);
+                            }
+                            $par = array_pop($m);
+
+                            $params = array();
+                            $arr    = preg_split('/(?<!\\\),/', $par);
+
+                            foreach ($arr as $a) {
+                                $a = trim($a);
+
+                                if (preg_match('/^_(\d+)$/', $a, $tmp)) {
+                                    $params[] = $args[($tmp[1] - 1)];
+                                } else {
+                                    $params[] = '\'' . $a . '\'';
+                                }
+                            }
+
+                            if (($cmd)&&(!method_exists($l10n, $cmd))) {
+                                die('unknown method ' . $cmd);
+                            } elseif ($cmd) {
+                                $code = $chr . ' . $this->' . $cmd . '(' . join(',', $params) . ') . ' . $chr;
+                            } else {
+                                $code = array_shift($params);
+                            }
+
+                            $txt = str_replace($str, $code, $txt);
+                        }
+                    }
+                    
+                }
+            } else {
+                $return = '$this->_(' . implode('', $args) . ')';
+            }
+            
+            return $return;
+        }
+        
+        protected static function _if($args) {
+            return sprintf(
+                '(%s ? %s : %s)', 
+                $args[0], 
+                $args[1], 
+                (count($args) > 2 ? $args[3] : $args[1])
+            );
+        }
+        
+        protected static function _mul($args) {
+            return '(' . implode(' * ', $args) . ')';
+        }
+        
+        protected static function _div($args) {
+            return '(' . implode(' / ', $args) . ')';
+        }
+        
+        protected static function _mod($args) {
+            return '(' . implode(' % ', $args) . ')';
+        }
+        
+        protected static function _add($args) {
+            return '(' . implode(' + ', $args) . ')';
+        }
+        
+        protected static function _sub($args) {
+            return '(' . implode(' - ', $args) . ')';
+        }
+        
+        protected static function _incr($args) {
+            return '(++' . $args[0] . ')';
+        }
+        
+        protected static function _decr($args) {
+            return '(--' . $args[0] . ')';
+        }
+        
+        protected static function _and($args) {
+            return '(' . implode(' && ', $args) . ')';
+        }
+
+        protected static function _or($args) {
+            return '(' . implode(' || ', $args) . ')';
+        }
+
+        protected static function _xor($args) {
+            return sprintf('(%d != %d)', !!$args[0], !!$args[1]);
+        }
+
+        protected static function _not($args) {
+            return '!' . $args[0];
+        }
+        
+        protected static function _lt($args) {
+            return '(' . implode(' < ', $args) . ')';
+        }
+        
+        protected static function _gt($args) {
+            return '(' . implode(' > ', $args) . ')';
+        }
+        
+        protected static function _eq($args) {
+            return '(' . implode(' == ', $args) . ')';
+        }
+        
+        protected static function _le($args) {
+            return '(' . implode(' <= ', $args) . ')';
+        }
+        
+        protected static function _ge($args) {
+            return '(' . implode(' >= ', $args) . ')';
+        }
+        
+        protected static function _ne($args) {
+            return '(' . implode(' == ', $args) . ')';
+        }
+    }
+}
