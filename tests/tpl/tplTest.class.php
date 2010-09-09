f5ad1256a85ffd1adb4ebf27ea53f4f5289c4cbd
Harald Lapp <harald@air.(none)>
2010-09-09 16:34:08 +0200

    - test

--- tests/tpl/tplTest.class.php
+++ tests/tpl/tplTest.class.php
@@ -3,6 +3,11 @@
 require_once('org.octris.core/app/autoloader.class.php');
 
 $tpl = new \org\octris\core\tpl();
+$tpl->registerMethod('gettext', function($msg) {
+    print "[$msg]\n";
+    
+    return $msg;
+}, array('min' => 1, 'max' => 1));
 $tpl->addSearchPath(dirname(__FILE__) . '/../../tests/tpl/compiler/');
-$tpl->render('tpl1.html');
+print $tpl->compile('tpl1.html');
 
