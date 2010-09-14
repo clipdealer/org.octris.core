290c77372ebd31cd1a0c18968f1a45c26e65f6b3
Harald Lapp <harald@octris.org>
2010-09-14 10:11:01 +0200

    - rewrite with cleaner code

--- libs/l10n.class.php
+++ libs/l10n.class.php
@@ -420,51 +420,32 @@ namespace org\octris\core {
          ****
          */
         {
-            $txt  = '\'' . str_replace("'", "\'", $txt) . '\'';
+            $txt     = '\'' . str_replace("'", "\'", $txt) . '\'';
+            $pattern = '/\[(?:(_\d+)|(?:([^,]+))(?:,(.*?))?(?<!\\\))\]/s';
 
-            $pattern = '/\[(?:(_\d+)|(?:([^,]+))(?:,(.*?))?(?<!\\\))\]/sie';
-            
-            if (preg_match_all($pattern, $txt, $match, PREG_SET_ORDER)) {
-                foreach ($match as $m) {
-                    $str = $m[0];
-                    $cmd = '';
-    
-                    if (isset($m[2])) {
-                        $cmd = $m[2];
-                        unset($m[2]);
-                    }
-                    $par = array_pop($m);
-    
-                    $params = array();
-                    $arr    = preg_split('/(?<!\\\),/', $par);
-    
-                    foreach ($arr as $a) {
-                        $a = trim($a);
-    
-                        if (preg_match('/^_(\d+)$/', $a, $tmp)) {
-                            $params[] = '$args[' . ($tmp[1] - 1) . ']';
-                        } else {
-                            $params[] = '\'' . $a . '\'';
-                        }
-                    }
-    
-                    if ($cmd && !method_exists($l10n, $cmd)) {
-                        die('unknown method ' . $cmd);
-                    } elseif ($cmd) {
-                        $code = $chr . ' . \org\octris\core\l10n::' . $cmd . '(' . join(',', $params) . ') . ' . $chr;
-                    } else {
-                        $code = $chr . ' . ' . array_shift($params) . ' . ' . $chr;
-                    }
-    
-                    $txt = str_replace($str, $code, $txt);
+            $txt = preg_replace_callback($pattern, function($m) {
+                $cmd = (isset($m[2]) ? $m[2] : '');
+                $tmp = preg_split('/(?<!\\\),/', array_pop($m));
+                $par = array();
+
+                foreach ($tmp as $t) {
+                    $par[] = (($t = trim($t)) && preg_match('/^_(\d+)$/', $t, $m)
+                                ? '$args[' . ($m[1] - 1) . ']'
+                                : '\'' . $t . '\'');
                 }
-        
-                $return = create_function('$args', 'return ' . $txt . ';');
+
+                $code = ($cmd != '' 
+                         ? '\' . $obj->' . $cmd . '(' . implode(',', $par) . ') . \''
+                         : '\' . ' . array_shift($par) . ' . \'');
+
+                return $code;
+            }, $txt, -1, $cnt = 0);
+
+            if ($cnt == 0) {
+                return function($args) use ($txt) { return $txt; };
             } else {
-                $return = function($args) use ($txt) { return $txt; };
+                return create_function('$obj, $args', 'return ' . $txt . ';');
             }
-            
-            return $return;
         }
 
         /****m* l10n/negotiateLanguage
