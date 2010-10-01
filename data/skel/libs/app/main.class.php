96b79a04ce57842098ade18572203cb2b384f62b
Harald Lapp <harald@octris.org>
2010-10-01 16:29:17 +0200

    - initial commit skeleton classes

---
+++ data/skel/libs/app/main.class.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace %%SKEL_NAMESPACE%%\app {
+    /****c* app/main
+     * NAME
+     *      main
+     * FUNCTION
+     *      main application. this class is only used to define an entry page - if it's the
+     *      first request to the web application and therefore no other page (next_page) 
+     *      is specified through the application state, this entry page is required.
+     * COPYRIGHT
+     *      copyright (c) %%SKEL_YEAR%% by %%SKEL_COMPANY%%
+     * AUTHOR
+     *      %%SKEL_AUTHOR%% <%%SKEL_EMAIL%%>
+     ****
+     */
+
+    class main extends \org\octris\core\app {
+        /****v* main/$entry_page
+         * SYNOPSIS
+         */
+        protected $entry_page = 'entry';
+        /*
+         * FUNCTION
+         *      page to use a entry point, if no "next_page" is specified through the
+         *      application state.
+         ****
+         */
+    }
+}
