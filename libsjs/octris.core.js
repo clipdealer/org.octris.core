5ae393f0d6eecc09faa6b9e9384075c5ca9988f4
Harald Lapp <harald@octris.org>
2010-12-10 23:37:43 +0100

    - initial commit core javascript libraries

---
+++ libsjs/octris.core.js
@@ -0,0 +1,21 @@
+/****h* octris/core
+ * NAME
+ *      octris.core.js
+ * FUNCTION
+ *      core javascript module
+ * COPYRIGHT
+ *      copyright (c) 2010 by Harald Lapp
+ * AUTHOR
+ *      Harald Lapp <harald@octris.org>
+ ****
+ */
+
+if (typeof octris == 'undefined') {
+    var octris = {};
+}
+
+;(function() {
+    if ('core' in octris) return;
+    
+    octris.core = {};
+})();
