e79120ee3053ca8b8b22c4b7f4e54a571c9728b0
Harald Lapp <harald.lapp@gmail.com>
2010-12-31 02:34:01 +0100

    - finished first version of project creation tool

--- tools/project/create.php
+++ tools/project/create.php
@@ -15,9 +15,29 @@
 
 require_once(__DIR__ . '/../../libs/app/cli.class.php');
 
+// helper function to check if file is binary
+function is_binary($file) {
+    $return = false;
+    
+    if (is_file($file) && ($fp = fopen($file, 'r'))) {
+        $blk = fread($fp, 512);
+        fclose($fp); 
+        
+        clearstatcache();
+        
+        $return = (
+            substr_count($blk, '^ -~', "^\r\n") / 512 > 0.3 ||
+            substr_count($blk, "\x00") > 0 
+        ); 
+    }
+    
+    return $return;
+}
+
 // initialization
 use \org\octris\core\app\cli as cli;
 use \org\octris\core\config as config;
+use \org\octris\core\tpl as tpl;
 
 $cfg = new config('org.octris.core');
 $prj = new config('org.octris.core', 'project.create');
@@ -44,15 +64,17 @@ foreach ($filter as $k => $v) {
 $prj->save();
 
 print "\n";
-$module = $prompt->get('module [%s]: ', '', true);
+$module = $prompt->get('module [%s]: ', 'mod', true);
+$year   = $prompt->get('year [%s]: ', date('Y'), true);
 
-if ($module == '') {
-    die("a module name is required!\n");
+if ($module == '' || $year == '') {
+    die("'module' and 'year' are required!\n");
 }
 
 $ns = '\\' . implode('\\', array_reverse(explode('.', $prj['info.domain']))) . '\\' . $module;
 
 $data = array_merge($prj->filter('info')->getArrayCopy(true), array(
+    'year'      => $year,
     'module'    => $module,
     'namespace' => $ns,
     'directory' => str_replace('\\', '.', ltrim($ns, '\\'))
@@ -65,4 +87,48 @@ if (is_dir($dir)) {
     die(sprintf("there seems to be already a project at '%s'\n", $dir));
 }
 
-print_r($data);
+// process skeleton and write project files
+$tpl = new tpl();
+$tpl->addSearchPath(__DIR__ . '/data/skel/');
+$tpl->setValues($data);
+
+$box = new tpl\sandbox();
+
+$src = __DIR__ . '/data/skel/';
+$len = strlen($src);
+
+mkdir($dir, 0755);
+
+$directories = array();
+$iterator    = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($src)
+);
+
+foreach ($iterator as $filename => $cur) {
+    $rel  = substr($filename, $len); 
+    $dst  = $dir . '/' . $rel;
+    $path = dirname($dst);
+    $base = basename($filename);
+    $ext  = preg_replace('/^\.?[^\.]+?(\..+|)$/', '\1', $base);
+    $base = basename($filename, $ext);
+    
+    if (substr($base, 0, 1) == '$' && isset($data[$base = ltrim($base, '$')])) {
+        // resolve variable in filename
+        $dst = $path . '/' . $data[$base] . $ext;
+    }
+    
+    if (!is_dir($path)) {
+        // create destination directory
+        mkdir($path, 0755, true);
+    }
+    
+    if (!is_binary($filename)) {
+        $cmp = $tpl->fetch($rel, tpl\sandbox::T_CONTEXT_TEXT);
+
+        file_put_contents($dst, $cmp);
+    } else {
+        copy($filename, $dst);
+    }
+}
+
+print "done.\n";
