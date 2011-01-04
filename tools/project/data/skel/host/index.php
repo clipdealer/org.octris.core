6db56b208ab9c69476050cad3a377cadef61bf8c
Harald Lapp <harald.lapp@gmail.com>
2011-01-04 19:03:18 +0100

    - documentation - changed handling of configuration

--- tools/project/data/skel/host/index.php
+++ tools/project/data/skel/host/index.php
@@ -1,23 +1,23 @@
 <?php
 
 namespace {{$namespace}} {
-    /****c* host/index
-     * NAME
-     *      index
-     * FUNCTION
-     *      application loader
-     * COPYRIGHT
-     *      copyright (c) {{$year}} by {{$company}}
-     * AUTHOR
-     *      {{$author}} <{{$email}}>
-     ****
+    /**
+     * Application loader.
+     *
+     * @octdoc      h:host/index
+     * @copyright   copyright (c) {{$year}} by {{$company}}
+     * @author      {{$author}} <{{$email}}>
      */
+    /**/
 
     // include core web application library
     require_once('org.octris.core/app/web.class.php');
     
     // load application configuration
-    \org\octris\core\config::load('{{$directory}}');
+    $registry = new \org\octris\core\registry::getInstance();
+    $registry->set('config', function() {
+        return new \org\octris\core\config('{{$directory}}');
+    }, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
 
     // run application
     app\main::getInstance()->process();
