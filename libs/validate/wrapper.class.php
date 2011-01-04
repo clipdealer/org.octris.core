5de1955f9488b9300ae73b41ba6dbac89f4ca99d
Harald Lapp <harald.lapp@gmail.com>
2011-01-04 19:28:25 +0100

    - documentation

--- libs/validate/wrapper.class.php
+++ libs/validate/wrapper.class.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace org\octris\core\validate {
-    /****c* validate/wrapper
-     * NAME
-     *      wrapper
-     * FUNCTION
-     *      enable validation for arrays
-     * COPYRIGHT
-     *      copyright (c) 2010 by Harald Lapp
-     * AUTHOR
-     *      Harald Lapp <harald.lapp@gmail.com>
-     ****
+    /**
+     * Enable validation for arrays.
+     *
+     * @octdoc      c:validate/wrapper
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
      */
-
-    class wrapper extends \org\octris\core\type\collection {
-        /****v* wrapper/$defaults
-         * SYNOPSIS
+    class wrapper extends \org\octris\core\type\collection
+    /**/
+    {
+        /**
+         * Default values for a property.
+         *
+         * @octdoc  v:wrapper/$defaults
+         * @var     array
          */
         protected $defaults = array(
             'isSet'     => true,
@@ -24,21 +24,16 @@ namespace org\octris\core\validate {
             'value'     => null,
             'tainted'   => null
         );
-        /*
-         * FUNCTION
-         *      default values for a parameter
-         ****
-         */
+        /**/
 
-        /****m* wrapper/__constructor
-         * SYNOPSIS
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:wrapper/__construct
+         * @param   array       $source         Source array to use for validation.
          */
-        public function __construct($source)
-        /*
-         * FUNCTION
-         *      constructor
-         ****
-         */
+        public function __construct(array $source)
+        /**/
         {
             if (($cnt = count($source)) > 0) {
                 $this->keys = array_keys($source);
@@ -56,21 +51,17 @@ namespace org\octris\core\validate {
             }
         }
 
-        /****m* wrapper/validate
-         * SYNOPSIS
+        /**
+         * Validate and untain a value.
+         *
+         * @octdoc  m:wrapper/validate
+         * @param   string      $name           Name of value to untaint.
+         * @param   string      $type           Type of value.
+         * @param   array       $options        Optional options for validating.
+         * @return  bool                        Returns TRUE if validation succeeded
          */
         public function validate($name, $type, array $options = array())
-        /*
-         * FUNCTION
-         *      validate and untained a value
-         * INPUTS
-         *      * $name (string) -- name of value to untaint
-         *      * $type (string) -- type of value
-         *      * $options (array) -- (optional) options for validating
-         * OUTPUTS
-         *      (bool) -- returns true, if validation succeededs
-         ****
-         */
+        /**/
         {
             if (($valid = isset($this->data[$name]))) {
                 if ($this->data[$name]->isTainted) {
@@ -92,19 +83,15 @@ namespace org\octris\core\validate {
             return $valid;
         }
 
-        /****m* wrapper/offsetGet
-         * SYNOPSIS
+        /**
+         * Return array entry of specified offset.
+         *
+         * @octdoc  m:wrapper/offsetGet
+         * @param   mixed       $offs           Offset to return.
+         * @return  stdClass                    Value.
          */
         public function offsetGet($offs)
-        /*
-         * FUNCTION
-         *      return array entry of specified offset
-         * INPUTS
-         *      * $offs (string) -- offset
-         * OUTPUTS
-         *      (stdClass) -- value
-         ****
-         */
+        /**/
         {
             if (($idx = array_search($offs, $this->keys, true)) !== false) {
                 $return = $this->data[$this->keys[$idx]];
@@ -117,19 +104,16 @@ namespace org\octris\core\validate {
         
             return $return;
         }
-            
-        /****m* wrapper/offsetSet
-         * SYNOPSIS
+
+        /**
+         * Overwrite an offset with an own value.
+         *
+         * @octdoc  m:wrapper/offsetSet
+         * @param   mixed       $offs           Offset to overwrite.
+         * @param   mixed       $value          Value to set.
          */
         public function offsetSet($offs, $value)
-        /*
-         * FUNCTION
-         *      overwrite collection's offsetSet to store value with meta data
-         * INPUTS
-         *      * $offs (mixed) -- offset to store value at
-         *      * $value (mixed) -- value to store
-         ****
-         */
+        /**/
         {
             $me = $this;
             
