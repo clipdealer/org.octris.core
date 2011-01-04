e1212bd908c1ceaff94441ad8d9993c382c4be79
Harald Lapp <harald.lapp@gmail.com>
2011-01-04 22:10:37 +0100

    - T_PRINT was changed to T_PRINTABLE

--- libs/validate.class.php
+++ libs/validate.class.php
@@ -32,18 +32,18 @@ namespace org\octris\core {
         /**
          * Available validation types.
          *
-         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_PATH, T_PRINT, T_XDIGIT
+         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_PATH, T_PRINTABLE, T_XDIGIT
          */
-        const T_ALPHA    = '\org\octris\core\validate\type\alpha';
-        const T_ALPHANUM = '\org\octris\core\validate\type\alphanum';
-        const T_BOOL     = '\org\octris\core\validate\type\bool';
-        const T_CALLBACK = '\org\octris\core\validate\type\callback';
-        const T_CHAIN    = '\org\octris\core\validate\type\chain';
-        const T_PATH     = '\org\octris\core\validate\type\path';
-        const T_PATTERN  = '\org\octris\core\validate\type\pattern';
-        const T_PRINT    = '\org\octris\core\validate\type\print';
-        const T_XDIGIT   = '\org\octris\core\validate\type\xdigit';
-        const T_URL      = '\org\octris\core\validate\type\url';
+        const T_ALPHA     = '\org\octris\core\validate\type\alpha';
+        const T_ALPHANUM  = '\org\octris\core\validate\type\alphanum';
+        const T_BOOL      = '\org\octris\core\validate\type\bool';
+        const T_CALLBACK  = '\org\octris\core\validate\type\callback';
+        const T_CHAIN     = '\org\octris\core\validate\type\chain';
+        const T_PATH      = '\org\octris\core\validate\type\path';
+        const T_PATTERN   = '\org\octris\core\validate\type\pattern';
+        const T_PRINTABLE = '\org\octris\core\validate\type\printable';
+        const T_XDIGIT    = '\org\octris\core\validate\type\xdigit';
+        const T_URL       = '\org\octris\core\validate\type\url';
         /**/
         
         /**
