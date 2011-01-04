c31f552aa584845786b140e46a524405d7dc8fbb
Harald Lapp <harald.lapp@gmail.com>
2011-01-04 19:13:35 +0100

    - work on settings class -- very platform dependend ... work in progress

---
+++ libs/app/web/settings.class.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace org\octris\core\app\web
+    /**
+     * user specific application settings handling
+     *
+     * @octdoc      c:core/settings
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald.lapp@gmail.com>
+     */
+    class settings extends \org\octris\core\validate\wrapper
+    /**/
+    {
+        /**
+         * Stores instance of settings object.
+         *
+         * @octdoc  v:settings/$instance
+         * @var     \org\octris\core\settings
+         */
+        protected $instance = null;
+        /**/
+        
+        /**
+         * Protected constructor to force using getInstance.
+         *
+         * @octdoc  m:settings/__construct
+         */
+        protected function __construct()
+        /**/
+        {
+        }
+        
+        /**
+         * Private clone method to prevent multiple instances of object.
+         *
+         * @octdoc  m:settings/__clone
+         */
+        private function __clone()
+        /**/
+        {
+        }
+        
+        /**
+         * Return instance of settings object.
+         *
+         * @octdoc  m:settings/getInstance
+         */
+        public function getInstance()
+        /**/
+        {
+            if (is_null(self::$instance)) {
+                self::$instance = new static();
+            }
+            
+            return self::$instance;
+        }
+    }
+}
