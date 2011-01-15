57f3465016542a3f459bc61e9558a859d3ce42d5
Harald Lapp <harald.lapp@gmail.com>
2011-01-15 15:24:52 +0100

    - class is not longer abstract - added method settype which should replace and enhance PHPs settype method

--- libs/type.class.php
+++ libs/type.class.php
@@ -8,8 +8,55 @@ namespace \org\octris\core {
      * @copyright   copyright (c) 2010-2011 by Harald Lapp
      * @author      Harald Lapp <harald@octris.org>
      */
-    abstract class type
+    class type
     /**/
     {
+        /**
+         * Cast input value to a specified type. In contrast to PHP's built-in settype function this one will try to cast arrays
+         * and objects to the template collection type. Additionally this function supports casting explicitly to the template
+         * collection type. All other types are handed over to PHP's built-in settype function.
+         *
+         * @octdoc  m:type/settype
+         * @param   mixed       $val            Value to cast.
+         * @param   string      $type           Type to cast to.
+         * @return  mixed                       Casted value.
+         */
+        public static function settype($val, $type)
+        /**/
+        {
+            $type = strtolower($type);
+
+            if ($type == 'array' || $type == 'object') {
+                if (is_object($val)) {
+                    if (($val instanceof collection) || ($val instanceof collection\Iterator) || ($val instanceof \ArrayIterator)) {
+                        $val = $val->getArrayCopy();
+                    } else {
+                        $val = (array)$val;
+                    }
+                } elseif (!is_array($val)) {
+                    $val = array($val);
+                }
+
+                if ($type == 'object') {
+                    $val = (object)$val;
+                }
+            } elseif ($type == 'collection') {
+                $val = new \org\octris\core\type\collection($val);
+            } elseif ($type == 'money') {
+                if (!is_object($val) || !($val instanceof \org\octris\core\type\money)) {
+                    // parameter is not a money object
+                    if (!is_numeric($money)) {
+                        // parameter is not a valid numeric value
+                        $val = 0;
+                    }
+                
+                    $val = new \org\octris\core\type\money($val);
+                }
+            } else {
+                \settype($val, $type);
+            }
+
+            return $val;
+        }
     }
 }
