741776990fc22a421c704b24b1cab9c21ce99fd2
Harald Lapp <harald@octris.org>
2011-01-27 20:57:00 +0100

    - removed leading namespace prefix. while in my understanding the documentation says, that the leading prefix is allowed to be used in a string (even though a dynamic namespace is always fully qualified), i had a nasty issue with this as soon as i had a second autoloader

--- libs/validate.class.php
+++ libs/validate.class.php
@@ -34,16 +34,16 @@ namespace org\octris\core {
          *
          * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_PATH, T_PRINTABLE, T_XDIGIT
          */
-        const T_ALPHA     = '\org\octris\core\validate\type\alpha';
-        const T_ALPHANUM  = '\org\octris\core\validate\type\alphanum';
-        const T_BOOL      = '\org\octris\core\validate\type\bool';
-        const T_CALLBACK  = '\org\octris\core\validate\type\callback';
-        const T_CHAIN     = '\org\octris\core\validate\type\chain';
-        const T_PATH      = '\org\octris\core\validate\type\path';
-        const T_PATTERN   = '\org\octris\core\validate\type\pattern';
-        const T_PRINTABLE = '\org\octris\core\validate\type\printable';
-        const T_XDIGIT    = '\org\octris\core\validate\type\xdigit';
-        const T_URL       = '\org\octris\core\validate\type\url';
+        const T_ALPHA     = 'org\octris\core\validate\type\alpha';
+        const T_ALPHANUM  = 'org\octris\core\validate\type\alphanum';
+        const T_BOOL      = 'org\octris\core\validate\type\bool';
+        const T_CALLBACK  = 'org\octris\core\validate\type\callback';
+        const T_CHAIN     = 'org\octris\core\validate\type\chain';
+        const T_PATH      = 'org\octris\core\validate\type\path';
+        const T_PATTERN   = 'org\octris\core\validate\type\pattern';
+        const T_PRINTABLE = 'org\octris\core\validate\type\printable';
+        const T_XDIGIT    = 'org\octris\core\validate\type\xdigit';
+        const T_URL       = 'org\octris\core\validate\type\url';
         /**/
         
         /**
