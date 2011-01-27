b43c5a96165b19087172d7e24d0c3d7509b98424
Harald Lapp <harald.lapp@gmail.com>
2011-01-28 00:53:47 +0100

    - main application controller for octsh

--- tools/octsh/app/main.class.php
+++ tools/octsh/app/main.class.php
@@ -1,9 +1,25 @@
 <?php
 
-namespace org\octris\core\tools\octsh\libs {
-    class main {
-        static function test() {
-            print "available\n";
-        }
+namespace org\octris\core\octsh\app {
+    /**
+     * Main application class for octris shell.
+     *
+     * @octdoc      c:app/main
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class main extends \org\octris\core\app\cli
+    /**/
+    {
+        /**
+         * Mapping of an option to an application class
+         *
+         * @octdoc  v:cli/$option_map
+         * @var     
+         */
+        protected $option_map = array(
+            '--help'    => 'org\octris\core\octsh\app\help'
+        );
+        /**/
     }
 }
