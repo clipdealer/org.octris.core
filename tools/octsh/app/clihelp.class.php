5e6085725b04e9fb7cfb950b143491dea69ebe61
Harald Lapp <harald@octris.org>
2011-01-28 16:31:22 +0100

    - started work on functionality for cli help page

--- tools/octsh/app/clihelp.class.php
+++ tools/octsh/app/clihelp.class.php
@@ -11,5 +11,31 @@ namespace org\octris\core\octsh\app {
     class clihelp extends \org\octris\core\app\cli\page
     /**/
     {
+        /**
+         * Prepare page
+         *
+         * @octdoc  m:page/prepare
+         * @param   \org\octris\core\app\page       $last_page      Instance of last called page.
+         * @param   string                          $action         Action that led to current page.
+         * @return  mixed                                           Returns either page to redirect to or null.
+         */
+        public function prepare(\org\octris\core\app\web\page $last_page, $action)
+        /**/
+        {
+        }
+
+        /**
+         * Abstract method definition
+         *
+         * @octdoc  m:page/render
+         * @abstract   
+         */
+        public function render()
+        /**/
+        {
+            print "help\n";
+
+            exit(0);
+        }
     }
 }
