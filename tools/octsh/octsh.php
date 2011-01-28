5a35991de372baa0ddbc9fa10e5f01cd0fc2a102
Harald Lapp <harald.lapp@gmail.com>
2011-01-28 23:28:17 +0100

    - enabled configuration loading - cleanup

--- tools/octsh/octsh.php
+++ tools/octsh/octsh.php
@@ -17,24 +17,11 @@ namespace org\octris\core\octsh {
     require_once('org.octris.core/app/cli.class.php');
     
     // load application configuration
-    // $registry = \org\octris\core\registry::getInstance();
-    // $registry->set('config', function() {
-    //     return new \org\octris\core\config('{{$directory}}');
-    // }, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
+    $registry = \org\octris\core\registry::getInstance();
+    $registry->set('config', function() {
+        return new \org\octris\core\config('org.octris.core');
+    }, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
 
     // run application
     app\main::getInstance()->process();
-    
-    // $info    = posix_getpwuid(posix_getuid());
-    // $history = $info['dir'] . '/.octris/octsh_history';
-    // 
-    // $prompt   = 'octsh> ';
-    // $readline = \org\octris\core\app\cli\readline::getInstance($history);
-    // 
-    // 
-    // do {
-    //     $return = $readline->readline($prompt);
-    //     
-    //     if ($return == 'quit') break;
-    // } while(true);
 }
