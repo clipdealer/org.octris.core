cb9fc18d2d07ae45610b3b479dcdaddd85fad866
Harald Lapp <harald@octris.org>
2011-01-28 14:07:15 +0100

    - started work on page class for cli help action

---
+++ tools/octsh/app/clihelp.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace org\octris\core\octsh\app {
+    /**
+     * Display help page for commandline parameters.
+     *
+     * @octdoc      c:app/clihelp
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class clihelp extends \org\octris\core\app\cli\page
+    /**/
+    {
+    }
+}
