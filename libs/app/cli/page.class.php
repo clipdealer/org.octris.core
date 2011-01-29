a05ddeb8387d58f1f5f9fcaf3787f04ac385a445
Harald Lapp <harald.lapp@gmail.com>
2011-01-29 22:25:19 +0100

    - work on validation related functionality

--- libs/app/cli/page.class.php
+++ libs/app/cli/page.class.php
@@ -21,13 +21,46 @@ namespace org\octris\core\app\cli {
         /**/
         
         /**
-         * Abstract method definition.
+         * Display and purge error messages.
+         *
+         * @octdoc  m:page/showErrors
+         */
+        public function showErrors()
+        /**/
+        {
+            if (count($this->errors > 0)) {
+                foreach ($this->errors as $error) {
+                    print "$error\n";
+                }
+                $this->errors = array();
+            }
+        }
+        
+        /**
+         * Abstract method definition. CLI application dialog.
          *
          * @octdoc  m:page/dialog
+         * @param   string          $action             Action that led to the dialog.
+         * @return  array                               Array with triggered command and command parameters.
+         * @abstract
          */
         abstract public function dialog($action);
         /**/
         
+        /**
+         * Apply validation ruleset to specified parameters.
+         *
+         * @octdoc  m:page/validate
+         * @param   \org\octris\core\app\cli\page   $last_page      Instance of last called page.
+         * @param   string                          $action         Action to select ruleset for.
+         * @param   array                           $parameters     Parameters to validate.
+         * @return  \org\octris\core\app\cli\page                   Returns page to display errors for.
+         */
+        public function validate(\org\octris\core\app\cli\page $last_page, $action, array $parameters = array())
+        /**/
+        {
+        }
+
         /**
          * Implements render page of core page class, because this method is
          * optional for cli application pages. Instead the abstract method 
