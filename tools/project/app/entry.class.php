cfa71e76525bbb181aecf64f925ef19de402484f
Harald Lapp <harald.lapp@gmail.com>
2011-01-30 17:17:54 +0100

    - initial commit of entry page for project tool

---
+++ tools/project/app/entry.class.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace org\octris\core\project\app {
+    /**
+     * Entry page.
+     *
+     * @octdoc      c:app/entry
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class entry extends \org\octris\core\app\cli\page
+    /**/
+    {
+        /**
+         * The entry points to which the current page should allow requests to have to be defined with this
+         * property.
+         *
+         * @octdoc  v:entry/$next_page
+         * @var     array
+         */
+        protected $next_pages = array(
+            'default' => '\org\octris\core\project\app\entry',
+        );
+        /**/
+
+        /**
+         * Prepare page
+         *
+         * @octdoc  m:entry/prepare
+         * @param   \org\octris\core\app\page       $last_page      Instance of last called page.
+         * @param   string                          $action         Action that led to current page.
+         * @return  mixed                                           Returns either page to redirect to or null.
+         */
+        public function prepare(\org\octris\core\app\page $last_page, $action)
+        /**/
+        {
+            print_r($_GET);
+        }
+
+        /**
+         * Dialog of entry page should never be called.
+         *
+         * @octdoc  m:entry/dialog
+         * @param   string                          $action         Action that led to current page.
+         */
+        public function dialog($action)
+        /**/
+        {
+            die('error!');
+        }
+    }
+}
