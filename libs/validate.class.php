d26449f7c5bfd5b99522ee2c7bce862c9df35f30
Harald Lapp <harald.lapp@gmail.com>
2011-01-30 22:10:21 +0100

    - added validation type

--- libs/validate.class.php
+++ libs/validate.class.php
@@ -32,7 +32,7 @@ namespace org\octris\core {
         /**
          * Available validation types.
          *
-         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_PATH, T_PRINTABLE, T_XDIGIT
+         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_PATH, T_PRINTABLE, T_PROJECT, T_XDIGIT
          */
         const T_ALPHA     = '\org\octris\core\validate\type\alpha';
         const T_ALPHANUM  = '\org\octris\core\validate\type\alphanum';
@@ -42,6 +42,7 @@ namespace org\octris\core {
         const T_PATH      = '\org\octris\core\validate\type\path';
         const T_PATTERN   = '\org\octris\core\validate\type\pattern';
         const T_PRINTABLE = '\org\octris\core\validate\type\printable';
+        const T_PROJECT   = '\org\octris\core\validate\type\project';
         const T_XDIGIT    = '\org\octris\core\validate\type\xdigit';
         const T_URL       = '\org\octris\core\validate\type\url';
         /**/
