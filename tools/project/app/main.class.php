091cc9889760a95e36691ec0450ba7127ccb5e28
Harald Lapp <harald.lapp@gmail.com>
2011-01-30 17:17:16 +0100

    - initial commit main application controller for project tool

---
+++ tools/project/app/main.class.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace org\octris\core\project\app {
+    /**
+     * Main application class for project tools.
+     *
+     * @octdoc      c:app/main
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class main extends \org\octris\core\app\cli
+    /**/
+    {
+        /**
+         * Entry page to use if no other page is loaded. To be overwritten by applications' main class.
+         *
+         * @octdoc  v:main/$entry_page
+         * @var     string
+         */
+        protected $entry_page = '\org\octris\core\project\app\entry';
+        /**/
+
+        /**
+         * Mapping of an option to an application class
+         *
+         * @octdoc  v:main/$option_map
+         * @var     
+         */
+        protected $option_map = array(
+            '--help'    => '\org\octris\core\project\app\clihelp'
+        );
+        /**/
+    }
+}
