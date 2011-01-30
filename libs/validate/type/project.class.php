9dc4ac53430178f7814b48c49516c590893b7620
Harald Lapp <harald.lapp@gmail.com>
2011-01-30 22:12:12 +0100

    - initial commit new validator for octris project names

---
+++ libs/validate/type/project.class.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace org\octris\core\validate\type {
+    /**
+     * Validator for octris project names. A qualified project name is:
+     *
+     * <TLD>.<COMPANY-NAME>.<MODULE-NAME>
+     *
+     * e.g.: com.clipdealer.core
+     *
+     * @octdoc      c:type/alpha
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class project extends \org\octris\core\validate\type
+    /**/
+    {
+        /**
+         * Validator implementation.
+         *
+         * @octdoc  m:project/validate
+         * @param   mixed       $value          Value to validate.
+         * @return  bool                        Returns true if value is valid.
+         */
+        public function validate($value)
+        /**/
+        {
+            $value .= '.';
+            
+            return preg_match(
+                '/^[a-z]{2,4}\.([a-z0-9]+([a-z0-9\-]*[a-z0-9]+|)\.){2}$/',
+                $value
+            );
+        }
+    }
+}
