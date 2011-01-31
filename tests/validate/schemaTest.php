c48c4891e74e617aa6001d2898f391d47fecfc1b
Harald Lapp <harald@octris.org>
2011-01-31 11:31:33 +0100

    - schema test needs to be rewritten

--- tests/validate/schemaTest.php
+++ tests/validate/schemaTest.php
@@ -5,30 +5,29 @@ require_once('org.octris.core/app/test.class.php');
 use \org\octris\core\app\test as test;
 
 class schemaTest extends PHPUnit_Framework_TestCase {
-    public function testSchema() {
-        // $_POST = array('p1' => 1, 'p2' => 2);
-        // 
-        // $v = new \org\octris\core\validate\schema(array(
-        //     'default' => array(                        // entry point, always required!
-        //         'type'          => 'object',
-        //         'properties'    => array(
-        //             'p1' => array(
-        //                 'type'  => 'digit'
-        //             ),
-        //             'p2' => array(
-        //                 'type'  => 'digit'
-        //             )
-        //         )
-        //     )
-        // ), 'array');
-        // // $r = (int)$v->validate($_POST);
-        // 
-        // print "p1->isSet: " . (int)$_POST['p1']->isSet . "\n";
-        // print "p1->validate: " . (int)$_POST['p1']->validate('digit') . "\n";
-        // 
-        // foreach ($_POST as $k => $v) {
-        //     print "$k";
-        //     print_r($v);
-        // }
+    public function testRequest() {
+        $data = array('p1' => 1, 'p2' => 2);
+
+        $wrapper = new \org\octris\core\validate\wrapper($data);
+        $schema  = new \org\octris\core\validate\schema(array(
+            'default' => array(                        // entry point, always required!
+                'type'          => 'object',
+                'properties'    => array(
+                    'p1' => array(
+                        'type'  => 'digit'
+                    ),
+                    'p2' => array(
+                        'type'  => 'digit'
+                    )
+                )
+            )
+        ), 'array');
+        
+        $r = (int)$schema->validate($wrapper);
+        
+        print "p1->isSet: " . (int)$wrapper['p1']->isSet . "\n";
+        print "p1->validate: " . (int)$wrapper['p1']->validate('digit') . "\n";
+        
+        print_r($wrapper);
     }
 }
