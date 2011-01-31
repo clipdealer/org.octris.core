8a432a12d9219576c552c344f0a89b25a08ac4aa
Harald Lapp <harald.lapp@gmail.com>
2011-01-31 22:50:21 +0100

    - added digit plugin

--- libs/validate.class.php
+++ libs/validate.class.php
@@ -32,13 +32,14 @@ namespace org\octris\core {
         /**
          * Available validation types.
          *
-         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_PATH, T_PRINTABLE, T_PROJECT, T_XDIGIT
+         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_DIGIT, T_PATH, T_PRINTABLE, T_PROJECT, T_XDIGIT
          */
         const T_ALPHA     = '\org\octris\core\validate\type\alpha';
         const T_ALPHANUM  = '\org\octris\core\validate\type\alphanum';
         const T_BOOL      = '\org\octris\core\validate\type\bool';
         const T_CALLBACK  = '\org\octris\core\validate\type\callback';
         const T_CHAIN     = '\org\octris\core\validate\type\chain';
+        const T_DIGIT     = '\org\octris\core\validate\type\digit';
         const T_PATH      = '\org\octris\core\validate\type\path';
         const T_PATTERN   = '\org\octris\core\validate\type\pattern';
         const T_PRINTABLE = '\org\octris\core\validate\type\printable';
