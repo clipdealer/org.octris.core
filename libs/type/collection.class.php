4b8e77660aa19de02ad3ea518c956a57ccc0fa60
Harald Lapp <harald.lapp@gmail.com>
2011-06-17 22:53:24 +0200

    - removed UTF-8 stuff

--- libs/type/collection.class.php
+++ libs/type/collection.class.php
@@ -312,65 +312,7 @@ namespace org\octris\core\type {
                 }
             }
         }
-        
-        /**
-         * UTF-8 encode collection values.
-         *
-         * @octdoc  m:collection/utf8Encode
-         * @return  \org\octris\core\type\collection    Encoded data.
-         */
-        public function utf8Encode()
-        /**/
-        {
-            $tmp = $this->data;
             
-            array_walk_recursive($tmp, function(&$v) {
-                if (is_string($v) && !preg_match('%^(?:  
-                [\x09\x0A\x0D\x20-\x7E] # ASCII  
-                | [\xC2-\xDF][\x80-\xBF] # non-overlong 2-byte  
-                | \xE0[\xA0-\xBF][\x80-\xBF] # excluding overlongs  
-                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte  
-                | \xED[\x80-\x9F][\x80-\xBF] # excluding surrogates  
-                | \xF0[\x90-\xBF][\x80-\xBF]{2} # planes 1-3  
-                | [\xF1-\xF3][\x80-\xBF]{3} # planes 4-15  
-                | \xF4[\x80-\x8F][\x80-\xBF]{2} # plane 16  
-                )*$%xs', $v)) {
-                    $v = utf8_encode($v);
-                }            
-            });
-
-            return new collection($tmp);
-        }
-
-        /**
-         * UTF-8 decode collection values.
-         *
-         * @octdoc  m:collection/utf8Decode
-         * @return  \org\octris\core\type\collection    Decoded data.
-         */
-        public function utf8Decode()
-        /**/
-        {
-            $tmp = $this->data;
-            
-            array_walk_recursive($tmp, function(&$v) {
-                if (is_string($v) && preg_match('%^(?:  
-                [\x09\x0A\x0D\x20-\x7E] # ASCII  
-                | [\xC2-\xDF][\x80-\xBF] # non-overlong 2-byte  
-                | \xE0[\xA0-\xBF][\x80-\xBF] # excluding overlongs  
-                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte  
-                | \xED[\x80-\x9F][\x80-\xBF] # excluding surrogates  
-                | \xF0[\x90-\xBF][\x80-\xBF]{2} # planes 1-3  
-                | [\xF1-\xF3][\x80-\xBF]{3} # planes 4-15  
-                | \xF4[\x80-\x8F][\x80-\xBF]{2} # plane 16  
-                )*$%xs', $v)) {
-                    $v = utf8_decode($v);
-                }            
-            });
-
-            return new collection($tmp);
-        }
-        
         /**
          * Return keys of collection.
          *
