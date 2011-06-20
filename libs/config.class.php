638ed369ff648c2c357f2b4bb1038d83a9ec1a66
Harald Lapp <harald@octris.org>
2011-06-20 17:37:09 +0200

    - source documentation

--- libs/config.class.php
+++ libs/config.class.php
@@ -101,6 +101,10 @@ namespace org\octris\core {
          * - T_PATH_ETC/config_local.yml
          * - ~/.octris/config.yml
          *
+         * whereat the confíguration file name -- in this example 'config' -- may be overwritten by the first parameter.
+         * The constant T_ETC_PATH is resolved by the value of the second parameter. By default T_ETC_PATH is resolved to
+         * the 'etc' path of the current running application.
+         *
          * @octdoc  m:config/_load
          * @param   string                              $name       Optional name of configuration file to load.
          * @param   string                              $module     Optional name of module to laod.
