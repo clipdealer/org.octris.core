2e74cf31b4f427635d21cb8b1c9be24c46056ace
Harald Lapp <harald.lapp@gmail.com>
2011-06-23 00:06:09 +0200

    - added skeleton for string tests

---
+++ tests/type/stringTest.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('org.octris.core/app/test.class.php');
+
+use \org\octris\core\app\test as test;
+
+class stringTest extends PHPUnit_Framework_TestCase {
+    public function testChr() {
+    }
+}
