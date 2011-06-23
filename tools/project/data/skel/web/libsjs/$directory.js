b565a4c1783b76aaaa3806dac251fece70632d74
Harald Lapp <harald.lapp@gmail.com>
2011-06-23 02:04:42 +0200

    - added copyright notice for skeletons

--- tools/project/data/skel/web/libsjs/$directory.js
+++ tools/project/data/skel/web/libsjs/$directory.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the '{{$directory}}' package.
+ *
+ * (c) {{$author}} <{{$email}}>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * Main javascript library for {{$directory}}.
  *
