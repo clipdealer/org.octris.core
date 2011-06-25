99133b38bc06dc050b19e7c0ba68ed73a5d483f4
Harald Lapp <harald.lapp@gmail.com>
2011-06-26 01:08:43 +0200

    - started work on formatting functionality

---
+++ libs/l10n/format.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\l10n\format {
+    /**
+     * Number formatter.
+     *
+     * @octdoc  f:format/number
+     * @param   int|float|\org\octris\core\type\money|\org\octris\core\type\money   $value          Value to format.
+     * @return  string                                                                              Formatted number value.
+     */
+    function number($value)
+    /**/
+    {
+        
+    }
+    
+    /**
+     * Money formatter.
+     *
+     * @octdoc  f:format/money
+     * @param   int|float|\org\octris\core\type\money|\org\octris\core\type\money   $value          Value to format.
+     * @param   string                                                              $currency       Optional currency in ISO 4217.
+     * @return  string                                                                              Formatted money value.
+     */
+    function money($value, $currency = null)
+    /**/
+    {
+        
+    }
+}
