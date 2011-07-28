a281fd89582b330abfafb0cdb14b41a7c7b6c0e7
Harald Lapp <harald.lapp@gmail.com>
2011-07-29 01:00:58 +0200

    - initial commit validator for base64 encoded data

---
+++ libs/validate/type/base64.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\validate\type {
+    /**
+     * Validator for base64 encoded data.
+     *
+     * @octdoc      c:type/base64
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class base64 extends \org\octris\core\validate\type
+    /**/
+    {
+        /**
+         * Validator implementation.
+         *
+         * @octdoc  m:base64/validate
+         * @param   mixed       $value          Value to validate.
+         * @return  bool                        Returns true if value is valid.
+         */
+        public function validate($value)
+        /**/
+        {
+            $value = preg_replace('/\s/', '', $value);
+            
+            return preg_match(
+                '/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/',
+                $value
+            );
+        }
+    }
+}
+
