e3c4a4e9edd25552537fc470613266102507c7cc
Harald Lapp <harald.lapp@gmail.com>
2011-07-29 01:01:17 +0200

    - cleanup, added new base64 validator

--- libs/validate.class.php
+++ libs/validate.class.php
@@ -32,20 +32,19 @@ namespace org\octris\core {
         /**
          * Available validation types.
          *
-         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BOOL, T_CALLBACK, T_DIGIT, T_PATH, T_PRINTABLE, T_PROJECT, T_XDIGIT, T_UTF8
+         * @octdoc  d:validate/T_ALPHA, T_ALPHANUM, T_BASE64, T_BOOL, T_DIGIT, 
+         *          T_PATH, T_PATTERN, T_PRINTABLE, T_PROJECT, T_UTF8, T_XDIGIT
          */
         const T_ALPHA     = '\org\octris\core\validate\type\alpha';
         const T_ALPHANUM  = '\org\octris\core\validate\type\alphanum';
+        const T_BASE64    = '\org\octris\core\validate\type\base64';
         const T_BOOL      = '\org\octris\core\validate\type\bool';
-        const T_CALLBACK  = '\org\octris\core\validate\type\callback';
-        const T_CHAIN     = '\org\octris\core\validate\type\chain';
         const T_DIGIT     = '\org\octris\core\validate\type\digit';
         const T_PATH      = '\org\octris\core\validate\type\path';
         const T_PATTERN   = '\org\octris\core\validate\type\pattern';
         const T_PRINTABLE = '\org\octris\core\validate\type\printable';
         const T_PROJECT   = '\org\octris\core\validate\type\project';
         const T_URL       = '\org\octris\core\validate\type\url';
-        const T_UTF8      = '\org\octris\core\validate\type\utf8';
         const T_XDIGIT    = '\org\octris\core\validate\type\xdigit';
         /**/
         
