dd4368d240aae1be02d90efce0f5cc5a991374e2
Harald Lapp <harald@octris.org>
2011-07-28 18:23:14 +0200

    - changed implementation to accept output pathes and resource pathes

--- libs/tpl/compress.class.php
+++ libs/tpl/compress.class.php
@@ -55,38 +55,49 @@ namespace org\octris\core\tpl {
          * @octdoc  m:compress/exec
          * @param   array       $files      Files to compress.
          * @param   string      $out        Name of path to store file in.
+         * @param   string      $inp        Name of base-path to lookup source file in.
          * @param   string      $type       Type of files to compress.
          * @param   array       $options    Optional additional options for yuicompressor.
          * @return  string                  Name of created file.
          */
-        protected static function exec($files, $out, $type, array $options = array())
+        protected static function exec($files, $out, $inp, $type, array $options = array())
         /**/
         {
-            array_walk($files, function(&$file) {
-                $file = escapeshellarg($file);
+            array_walk($files, function(&$file) use ($inp) {
+                $file = escapeshellarg($inp . '/' . $file);
             });
-            $options = array_merge(self::$defaults[$type], $options, array('type' => $type));
-            $options = array_map(function($k, $v) {
-                return "--$k " . escapeshellarg($v);
-            }, array_keys($options), array_values($options));
+
+            $tmp = tempnam('/tmp', 'oct');
             
-            $tmp = tempnam('/tmp', 'yui');
-            $cmd = sprintf(
-                'cat %s | java -jar /opt/yuicompressor/yuicompressor.jar %s -o %s 2>&1',
-                implode(' ', $files),
-                implode(' ', $options),
-                $tmp
-            );
+            if (self::$enabled) {
+                $options = array_merge(self::$defaults[$type], $options, array('type' => $type));
+                $options = array_map(function($k, $v) {
+                    return "--$k " . escapeshellarg($v);
+                }, array_keys($options), array_values($options));
+
+
+                $cmd = sprintf(
+                    'cat %s | java -jar /opt/yuicompressor/yuicompressor.jar %s -o %s 2>&1',
+                    implode(' ', $files),
+                    implode(' ', $options),
+                    $tmp
+                );
+            } else {
+                $cmd = sprintf(
+                    'cat %s > %s 2>&1',
+                    implode(' ', $files),
+                    $tmp
+                );
+            }
+
+            $ret = array(); $ret_val = 0;
+            exec($cmd, $ret, $ret_val);
+
+            $md5  = md5_file($tmp);
+            $name = $md5 . '.' . $type;
+            rename($tmp, $out . '/' . $name);
             
-            // print "$cmd";
-            // exec($cmd, $out = array(), $ret_val = 0);
-            // 
-            // print "[$ret_val]";
-            // 
-            // $md5 = md5_file($tmp);
-            // rename($tmp, $out . '/' . $md5 . '.' . $type);
-            
-            return $tmp;
+            return $name;
         }
         
         /**
@@ -95,12 +106,13 @@ namespace org\octris\core\tpl {
          * @octdoc  m:compress/compressCSS
          * @param   array       $files      Array of files to load and compress.
          * @param   string      $out        Name of path to store file in.
+         * @param   string      $inp        Name of base-path to lookup css file in.
          * @return  string                  Filename of compressed CSS files.
          */
-        public static function compressCSS(array $files, $out)
+        public static function compressCSS(array $files, $out, $inp)
         /**/
         {
-            return self::exec($files, $out, 'css');
+            return self::exec($files, $out, $inp, 'css');
         }
 
         /**
@@ -109,12 +121,13 @@ namespace org\octris\core\tpl {
          * @octdoc  m:compress/compressJS
          * @param   array       $files      Array of files to load and compress.
          * @param   string      $out        Name of path to store file in.
+         * @param   string      $inp        Name of base-path to lookup javascript file in.
          * @return  string                  Filename of compressed Javascript files.
          */
-        public static function compressJS(array $files, $out)
+        public static function compressJS(array $files, $out, $inp)
         /**/
         {
-            return self::exec($files, $out, 'js');
+            return self::exec($files, $out, $inp, 'js');
         }
 
         /**
@@ -122,11 +135,11 @@ namespace org\octris\core\tpl {
          *
          * @octdoc  m:compress/process
          * @param   string      $tpl        Template to compress
-         * @param   string      $out_js     Path to output compressed Javascript in.
-         * @param   string      $out_css    Path to output compressed CSS in.
+         * @param   string      $out        Array of output pathes.
+         * @param   string      $inp        Array of input pathes.
          * @return  string                  Processed template.
          */
-        public function process($tpl, $out_js, $out_css)
+        public function process($tpl, array $out, array $inp)
         /**/
         {
             // methods purpose is to collection script/style blocks and extract all included external files. the function
@@ -152,20 +165,26 @@ namespace org\octris\core\tpl {
             };
 
             // process external javascripts
+            $out_js = $out['js'];
+            $inp_js = $inp['js'];
+            
             $process(
                 '<script[^>]+src="(libsjs/\d+.js)"[^>]*></script>', 
                 '<script type="text/javascript" src="/libsjs/%s"></script>',
-                function($files) use ($out_js) {
-                    return \org\octris\core\tpl\compress::compressJS($files, $out_js);
+                function($files) use ($out_js, $inp_js) {
+                    return \org\octris\core\tpl\compress::compressJS($files, $out_js, $inp_js);
                 }
             );
 
             // process external css
+            $out_css = $out['css'];
+            $inp_css = $inp['css'];
+            
             $process(
                 '<link[^>]*? href="(?!https?://)([^"]+\.css)"[^>]*/>',
                 '<link rel="stylesheet" href="/styles/%s" type="text/css" />',
-                function($files) use ($out_css) {
-                    return \org\octris\core\tpl\compress::compressCSS($files, $out_css);
+                function($files) use ($out_css, $inp_css) {
+                    return \org\octris\core\tpl\compress::compressCSS($files, $out_css, $inp_css);
                 }
             );
             
