cfdf34f4c96811f9fc3d8c453c7b527f20c1a276
Harald Lapp <harald.lapp@gmail.com>
2011-08-27 19:33:28 +0700

    - started work on class for session handling

---
+++ libs/app/web/session.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\app\web {
+    /**
+     * Session handler base class.
+     *
+     * @octdoc      c:web/session
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class session
+    /**/
+    {
+
+    }
+
+    session_start();
+}
