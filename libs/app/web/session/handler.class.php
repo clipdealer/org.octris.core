984e654df4635c45c340a16bef602d94ab173809
Harald Lapp <harald.lapp@gmail.com>
2011-08-30 13:22:01 +0700

    - removes obsolete functionality

--- libs/app/web/session/handler.class.php
+++ libs/app/web/session/handler.class.php
@@ -21,15 +21,6 @@ namespace org\octris\core\app\web\session {
     /**/
     {
         /**
-         * Session data.
-         *
-         * @octdoc  v:handler/$data
-         * @var     array
-         */
-        protected $data = array();
-        /**/
-
-        /**
          * Constructor.
          *
          * @octdoc  m:handler/__construct
@@ -43,70 +34,6 @@ namespace org\octris\core\app\web\session {
          * prevent cloning
          */
         private function __clone() {}
-
-        /**
-         * Store a value in session.
-         *
-         * @octdoc  m:handler/setValue
-         * @param   string          $name               Name of property to set.
-         * @param   mixed           $value              Value to store in session.
-         */
-        public function setValue($name, $value)
-        /**/
-        {
-            $this->data[$name] = $value;
-        }
-
-        /**
-         * Return a value stored in session.
-         *
-         * @octdoc  m:handler/getValue
-         * @param   string          $name               Name of property to return value of.
-         */
-        public function getValue($name)
-        /**/
-        {
-            return $this->data[$name];
-        }
-
-        /**
-         * Test if a stored property exists.
-         *
-         * @octdoc  m:handler/isExist
-         * @param   string          $name               Name of property to test.
-         */
-        public function isExist($name)
-        /**/
-        {
-            return (array_key_exists($this->data[$name]));
-        }
-
-        /**
-         * Handle reading session data and storing it into internal data array, so that the
-         * '$_SESSION' superglobal can be ommited.
-         *
-         * @octdoc  m:handler/readData
-         * @param   string      $id                     Id of session to read.
-         */
-        public final function readData($id)
-        /**/
-        {
-            $this->data = $this->read($id);
-        }
-
-        /**
-         * Handle writing session data from internal data array, so that the '$_SESSION' superglobal
-         * can be ommited.
-         *
-         * @octdoc  m:handler/writeData
-         * @param   string      $id                     Id of session to write.
-         * @param   array       $data                   Session data to write.
-         */
-        public final function writeData($id, array $data)
-        /**/
-        {
-            $this->write($id, $this->data);
-        }
 
         /**
          * Open session.
