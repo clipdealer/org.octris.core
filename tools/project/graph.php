4d5fdd6369056b94b3e93372f9ef5564ed3cc316
Harald Lapp <harald.lapp@gmail.com>
2011-09-02 01:00:25 +0700

    - initial commit tool for creating page graphs

---
+++ tools/project/graph.php
@@ -0,0 +1,36 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\project {
+    /**
+     * Tool for creating a page graph of a project.
+     *
+     * @octdoc      h:tools/project
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    /**/
+
+    $_ENV['OCTRIS_APP'] = 'org.octris.core';
+
+    // include core cli application library
+    require_once('org.octris.core/app/cli.class.php');
+
+    // load application configuration
+    $registry = \org\octris\core\registry::getInstance();
+    $registry->set('config', function() {
+        return new \org\octris\core\config('org.octris.core');
+    }, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
+
+    // run application
+    app\main::getInstance()->invoke(new app\graph());
+}
