b8897bac79286ff8838cce72bd0233ed9d50229b
Harald Lapp <harald.lapp@gmail.com>
2011-09-02 01:00:56 +0700

    - initial commit app controller class for page graph tool

---
+++ tools/project/app/graph.class.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\project\app {
+    use \org\octris\core\app as app;
+    use \org\octris\core\config as config;
+    use \org\octris\core\validate as validate;
+
+    /**
+     * Create page graph of project.
+     *
+     * @octdoc      c:app/graph
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class graph extends \org\octris\core\app\cli\page
+    /**/
+    {
+        /**
+         * Name of project
+         *
+         * @octdoc  v:graph/$project
+         * @var     string
+         */
+        protected $project;
+        /**/
+
+        /**
+         * Prepare page
+         *
+         * @octdoc  m:graph/prepare
+         * @param   \org\octris\core\app\page       $last_page      Instance of last called page.
+         * @param   string                          $action         Action that led to current page.
+         * @return  mixed                                           Returns either page to redirect to or null.
+         */
+        public function prepare(\org\octris\core\app\page $last_page, $action)
+        /**/
+        {
+            // import project name
+            $args = \org\octris\core\provider::access('args');
+
+            if (($project = $args->getValue('p', \org\octris\core\validate::T_PROJECT))) {
+                $this->project = $project;
+            } else {
+                die("usage: ./graph.php -p project-name\n");
+            }
+        }
+
+        /**
+         * Validate parameters.
+         *
+         * @octdoc  m:graph/validate
+         * @param   string                          $action         Action that led to current page.
+         * @return
+         */
+        public function validate()
+        /**/
+        {
+            return true;
+        }
+
+        /**
+         * Implements dialog.
+         *
+         * @octdoc  m:graph/dialog
+         * @param   string                          $action         Action that led to current page.
+         */
+        public function dialog($action)
+        /**/
+        {
+        }
+
+        /**
+         * Render.
+         *
+         * @octdoc  m:graph/render
+         */
+        public function render()
+        /**/
+        {
+            $analyze = function($page) use (&$analyze) {
+                static $processed = array();
+
+                if (in_array($page, $processed)) {
+                    return;
+                }
+
+                $processed[] = $page;
+
+                try {
+                    $class = new \ReflectionClass($page);
+                } catch(\Exception $e) {
+                    return;
+                }
+
+                if (!$class->hasProperty('next_pages')) {
+                    return;
+                }
+
+                $tmp = $class->getProperty('next_pages');
+                $tmp->setAccessible(true);
+
+                $obj = new $page();
+                $pages = $tmp->getValue($obj);
+
+                asort($pages);
+
+                // process next_pages
+                foreach ($pages as $k => $v) {
+                    printf(
+                        "\"%s\" -> \"%s\" [label=%s];\n",
+                        $page,
+                        $v,
+                        ($k == '' ? 'default' : $k)
+                    );
+
+                    $analyze("\\$v");
+                }
+            };
+
+            print "digraph unix {\nsize=\"10,10\"\nnode [color=lightblue2, style=filled];\n";
+            print "rankdir=LR;\n";
+
+            $entry = '\\' . str_replace('.', '\\', $this->project) . '\\app\\entry';
+
+            $analyze($entry);
+
+            print "}\n";
+        }
+    }
+}
