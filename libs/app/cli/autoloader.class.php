0b13be9837019159d1aaaaf979c87b2f4d456ff4
Harald Lapp <harald.lapp@gmail.com>
2011-09-02 00:58:56 +0700

    - prevent \ErrorException in case the include-file is not found

--- libs/app/cli/autoloader.class.php
+++ libs/app/cli/autoloader.class.php
@@ -12,7 +12,7 @@
 namespace org\octris\core\app\cli {
     use \org\octris\core\validate as validate;
     use \org\octris\core\provider as provider;
-    
+
     /**
      * Class Autoloader. This is a special autoloader for cli applications. It's required because they may
      * have libraries installed in a libs subdirectory of the clis' directory located in the tools directory.
@@ -34,12 +34,15 @@ namespace org\octris\core\app\cli {
         /**/
         {
             $env = provider::access('env');
-            
+
             $pkg = preg_replace('|\\\\|', '/', preg_replace('|\\\\|', '.', ltrim($classpath, '\\\\'), 2)) . '.class.php';
 
             $pkg = $env->getValue('OCTRIS_BASE', validate::T_PATH) . '/tools/' . $pkg;
 
-            include_once($pkg);
+            try {
+                include_once($pkg);
+            } catch(\Exception $e) {
+            }
         }
     }
 
