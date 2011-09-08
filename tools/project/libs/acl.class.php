f6067acd156055a17bf3748a97e781058e8b3416
Harald Lapp <harald.lapp@gmail.com>
2011-09-08 12:01:52 +0700

    - implemented methods for manipulating ACL

--- tools/project/libs/acl.class.php
+++ tools/project/libs/acl.class.php
@@ -22,21 +22,34 @@ namespace org\octris\core\project\libs {
     {
         /**
          * "Merge" resources, that means: remove all resources, that are not present in
-         * the specified list of resources and add new resources for entries from the
-         * specified list, that do not already exist.
+         * the specified list of resources, add new resources for entries from the
+         * specified list, that do not already exist and remove policies for resources,
+         * that do not longer exist.
          *
-         * @octdoc  m:acl/diffResources
+         * @octdoc  m:acl/merge
          * @param   array       $resources      List of resources to include / exclude.
+         * @return  array                       Status information about removed / added resources, policies, etc.
          */
-        public function mergeResources(array $resources)
+        public function merge(array $resources)
         /**/
         {
+            $result = array(
+                'del_resources' => 0,       // resources deleted
+                'add_resources' => 0,       // resources added
+                'del_policies'  => 0,       // policies deleted
+                'prc_roles'     => 0,       // processed roles
+                'tot_resources' => 0,       // total resources
+                'tot_policies'  => 0,       // total policies
+                'tot_roles'     => 0,       // total roles
+            );
+
             // remove not longer existing resources
             $tmp = $this->resources;
 
-            array_map(function($k) use (&$tmp, $resources) {
+            array_map(function($k) use (&$tmp, &$result, $resources) {
                 if (!isset($resources[$k])) {
                     unset($tmp[$k]);
+                    ++$result['del_resources'];
                 }
             }, array_keys($tmp));
 
@@ -44,46 +57,129 @@ namespace org\octris\core\project\libs {
             foreach ($resources as $k => $v) {
                 if (!isset($tmp[$k])) {
                     $tmp[$k] = new \org\octris\core\auth\acl\resource($v['name'], $v['actions']);
+                    ++$result['add_resources'];
                 }
             }
 
-            // set updates list
+            $result['tot_resources'] = count($tmp);
+
             ksort($tmp);
 
             $this->resources = $tmp;
+
+            // remove not longer existing policies
+            foreach ($this->roles as $role) {
+                $reflection = new \ReflectionClass('\org\octris\core\auth\acl\role');
+                $property   = $reflection->getProperty('policies');
+                $property->setAccessible(true);
+
+                $policies = $property->getValue($role);
+
+                $flag = false;
+
+                array_map(function($k) use (&$policies, &$result, &$flag, $resources) {
+                    if (!isset($resources[$k])) {
+                        unset($policies[$k]);
+                        ++$result['del_policies'];
+                        $flag = true;
+                    }
+                }, array_keys($policies));
+
+                if ($flag) {
+                    ++$result['prc_roles'];
+
+                    $property->setValue($role, $policies);
+                }
+
+                $result['tot_policies'] += count($policies);
+            }
+
+            $result['tot_roles'] = count($this->roles);
+
+            return $result;
         }
 
         /**
-         * "Merge" roles, that means: remove all roles, that are not present in
-         * the specified list of roles and add new rolesfor entries from the
-         * specified list, that do not already exist.
+         * Save ACL.
          *
-         * @octdoc  m:acl/mergeRoles
-         * @param   array       $roles          List of roles to include / exclude.
+         * @octdoc  m:acl/save
+         * @param   string              $filename           Filename to save ACL to.
          */
-        public function mergeRoles(array $roles)
+        public function save($filename)
         /**/
         {
-            // remove not longer existing roles
-            $tmp = $this->roles;
+            $result = array(
+                'roles'     => array(),
+                'resources' => array(),
+                'policies'  => array()
+            );
 
-            array_map(function($k) use (&$tmp, $roles) {
-                if (!isset($roles[$k])) {
-                    unset($tmp[$k]);
-                }
-            }, array_keys($tmp));
+            // export roles
+            foreach ($this->roles as $name => $role) {
+                $reflection = new \ReflectionClass('\org\octris\core\auth\acl\role');
+                $property   = $reflection->getProperty('parents');
+                $property->setAccessible(true);
 
-            // add new roles
-            foreach ($roles as $k => $v) {
-                if (!isset($tmp[$k])) {
-                    $tmp[$k] = new \org\octris\core\auth\acl\role($v['name']);
-                }
+                $tmp = array_map(function($v) {
+                    return (string)$v;
+                }, $property->getValue($role));
+
+                $result['roles'][] = array(
+                    $name => $tmp
+                );
             }
 
-            // set updates list
-            ksort($tmp);
+            // export resources
+            foreach ($this->resources as $name => $resource) {
+                $reflection = new \ReflectionClass('\org\octris\core\auth\acl\resource');
+                $property   = $reflection->getProperty('actions');
+                $property->setAccessible(true);
 
-            $this->roles = $tmp;
+                $actions = $property->getValue($resource);
+
+                $result['resources'][] = array(
+                    $name => $actions
+                );
+            }
+
+            // export policies
+            foreach ($this->resources as $name => $resource) {
+                $policy = $resource->getPolicy();
+
+                $tmp = array(
+                    'default' => ($policy == self::T_ALLOW
+                                    ? 'ALLOW'
+                                    : 'DENY')
+                );
+
+                $reflection = new \ReflectionClass('\org\octris\core\auth\acl\resource');
+                $property   = $reflection->getProperty('actions');
+                $property->setAccessible(true);
+
+                if (count($actions = $property->getValue($resource)) > 0) {
+                    $tmp['actions'] = array_map(function($v) {
+                        return array($v => array());
+                    }, $actions);
+
+                    foreach ($this->roles as $role) {
+                        foreach ($tmp['actions'] as &$action) {
+                            $key = key($action);
+
+                            if (!is_null($policy = $role->getPolicy($resource, $key))) {
+                                $action[$key][(string)$role] = ($policy == self::T_ALLOW
+                                                                ? 'ALLOW'
+                                                                : 'DENY');
+                            }
+                        }
+                    }
+                }
+
+                $result['resources'][] = array(
+                    $name => $tmp
+                );
+            }
+
+            file_put_contents($filename, yaml_emit($result));
         }
     }
 }
