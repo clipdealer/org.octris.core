c2250b79333caa07688d8420f3a0b99ee08f2c6e
Harald Lapp <harald.lapp@gmail.com>
2011-09-08 14:12:41 +0700

    - initial commit skeleton for implementing tab panes functionality for a page

---
+++ libs/app/web/page/panes.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\app\web\page {
+    /**
+     * Implements functionality for using tab panes in a page.
+     *
+     * @octdoc      t:page/panes
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    trait panes
+    /**/
+    {
+    }
+}
