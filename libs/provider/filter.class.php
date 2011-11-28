41d950a3e84de971ffa25781a8cabb98a2421a1d
Harald Lapp <harald.lapp@gmail.com>
2011-11-29 00:48:02 +0100

    initial commit provider field filter

---
+++ libs/provider/filter.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\provider {
+    /**
+     * Implements FilterIterator for filtering provider properties.
+     *
+     * @octdoc      c:config/filter
+     * @copyright   copyright (c) 2011 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     **
+     */
+
+    class filter extends \FilterIterator {
+        /**
+         * Prefix to use as filter.
+         *
+         * @octdoc  v:filter/$prefix
+         * @var     string
+         */
+        private $prefix = '';
+        /**/
+
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:filter/__construct
+         * @param   string          $prefix         Prefix to filter for.
+         * @param   array           $keys           Array of property names.
+         */
+        public function __construct($prefix, array $keys)
+        /**/
+        {
+            parent::__construct(new ArrayIterator($keys));
+
+            $this->prefix = $prefix;
+            $this->rewind();
+        }
+
+        /**
+         * Filter implementation.
+         *
+         * @octdoc  m:filter/accept
+         * @return  bool        Returns TRUE, if element should be part of result.
+         */
+        public function accept()
+        /**/
+        {
+            return (substr($this->key(), 0, strlen($this->prefix)) == $this->prefix);
+        }
+    }
+}
