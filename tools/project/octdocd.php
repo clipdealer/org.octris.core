18efe75c5ca17b32a918af022c1dfe3b322e2fbc
Harald Lapp <harald.lapp@gmail.com>
2012-02-08 12:41:12 +0100

    test started server using ps

--- tools/project/octdocd.php
+++ tools/project/octdocd.php
@@ -45,9 +45,14 @@ if ($sapi == 'cli') {
     $host = '127.0.0.1';
     $port = '8888';
 
-    exec(sprintf('((%s -d output_buffering=on -S %s:%s %s 1>/dev/null 2>&1 &) &)', $cmd, $host, $port, $router));
+    exec(sprintf('((%s -d output_buffering=on -S %s:%s %s 1>/dev/null 2>&1 &) &)', $cmd, $host, $port, $router), $out, $ret);
+    exec('ps ax | grep octdocd.php | grep -v grep | grep "\-S"', $out, $ret);
 
-    die(sprintf("octdocd server started on '%s:%s'\n", $host, $port));
+    if ($ret !== 0) {
+        die(sprintf("unable to start webserver on '%s:%s'\n", $host, $port));
+    } else {
+        die(sprintf("octdocd server started on '%s:%s'\n", $host, $port));
+    }
 } elseif ($sapi != 'cli-server') {
     die("unable to execute octdocd server in environment '$sapi'\n");
 }
