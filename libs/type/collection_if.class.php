06e7b6f605bb50561c5beecbeff5089b33045abc
Harald Lapp <harald.lapp@gmail.com>
2012-03-07 02:02:39 +0100

    initial commit interfaces for collection and iterator

---
+++ libs/type/collection_if.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\type {
+    /**
+     * Interface for implementing collections.
+     *
+     * @octdoc      i:type/collections_if
+     * @copyright   copyright (c) 2012 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    interface collection_if 
+    /**/
+    {
+    }
+}
