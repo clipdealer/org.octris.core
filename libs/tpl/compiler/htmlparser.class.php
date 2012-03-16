0ca6f9c85e6fe1c74c09eee67021ecc9cc98ad38
Harald Lapp <harald.lapp@gmail.com>
2012-03-16 22:54:38 +0100

    work on rewrite of html parser

--- libs/tpl/compiler/htmlparser.class.php
+++ libs/tpl/compiler/htmlparser.class.php
@@ -11,111 +11,103 @@
 
 namespace org\octris\core\tpl\compiler {
     /**
-     * HTML Template parser for auto-escaping. Note, that the current parser is very
-     * simple and has probably security issues. Auto-escaping is therefore to be
-     * considered as experimental.
-     * 
-     * The following pages hold interesting information for escaping in HTML files:
-     * 
-     * * http://www.w3.org/TR/html4/index/attributes.html
-     * * http://www.w3.org/TR/html4/interact/scripts.html
-     * * http://tools.ietf.org/html/draft-hoehrmann-javascript-scheme-03
-     * 
+     * HTML Parser for auto-escaping functionality.
+     *
      * @octdoc      c:compiler/htmlparser
      * @copyright   copyright (c) 2012 by Harald Lapp
      * @author      Harald Lapp <harald@octris.org>
      */
-    class htmlparser
+    class htmlparser implements \Iterator 
     /**/
     {
         /**
-         * HTML-Parser tokens.
+         * Parser states.
          *
-         * @octdoc  d:htmlparser/T_...
+         * @octdoc  p:htmlparser/T_...
          */
-        const T_DATA        = 1;
-
-        const T_TAG         = 2;
-        const T_TAG_INNER   = 3;
-        
-        const T_ATTRIBUTE   = 4;
-        
-        const T_URI         = 10;
-
-        const T_JAVASCRIPT	= 30;
-        const T_CSS         = 40;
-        
-        const T_COMMAND     = 50;
+        const T_DATA            = 1;
+        const T_COMMAND         = 2;
+    
+        const T_TAG_START       = 10;
+        const T_TAG_END_OPEN    = 11;
+        const T_TAG_END_CLOSE   = 12;
+        const T_TAG_NAME        = 13;
+        const T_TAG_CLOSE       = 14;
+        const T_TAG_ATTRIBUTE   = 15;
+    
+        const T_COMMENT_OPEN    = 30;
+        const T_COMMENT_CLOSE   = 31;
+    
+        const T_CDATA_OPEN      = 40;
+        const T_CDATA_CLOSE     = 41;
         /**/
-
+    
         /**
-         * Patterns for HTML parser.
+         * Parser patterns
          *
          * @octdoc  p:htmlparser/$patterns
          * @var     array
          */
         protected static $patterns = array(
-            // data state
-            self::T_DATA => array(
-                '/\{\{(.*?)\}\}/'               => self::T_COMMAND,
-                '/<script.*?>/i'                => self::T_JAVASCRIPT,
-                '/<style.*?>/i'                 => self::T_CSS,
-                '/<([a-z]+)(!? \/|)>/i'         => self::T_DATA,
-                '/<\/[a-z]+>/i'                 => self::T_DATA,
-                '/<([a-z]+)/i'                  => self::T_TAG_INNER,
-                '/<\/?/i'                       => self::T_TAG,
+            self::T_TAG_START       => '/</',
+            self::T_TAG_END_OPEN    => '/\s*>/',
+            self::T_TAG_END_CLOSE   => '/\s*\/>/',
+            self::T_TAG_NAME        => '/(_c_[a-f0-9]+_|(?i:[a-z]+))/',
+            self::T_TAG_CLOSE       => '/\/(_c_[a-f0-9]+_|(?i:[a-z]+))>/',
+            self::T_TAG_ATTRIBUTE   => '/(?<=\s)(_c_[a-f0-9]+_|(?i:[a-z:_][a-z:_.-]*))=\"(.*?)(?!\\\\)\"/',
+        
+            self::T_COMMENT_OPEN    => '/<!--/',
+            self::T_COMMENT_CLOSE   => '/-->/',
+        
+            self::T_CDATA_OPEN      => '/<!\[CDATA\[/i',
+            self::T_CDATA_CLOSE     => '/\]\]/',
+        
+            self::T_COMMAND         => '/\b_c_[a-f0-9]+_\b/',
+        );
+        /**/
+    
+        /**
+         * Parser rules.
+         *
+         * @octdoc  p:htmlparser/$rules
+         * @var     array
+         */
+        protected static $rules = array(
+            self::T_DATA            => array(
+                self::T_TAG_START,
+                self::T_COMMAND
             ),
-            
-            // tag (name of a tag) state
-            self::T_TAG => array(
-                '/[a-z]+/i'                     => self::T_TAG_INNER,
-                '/\{\{(.*?)\}\}/'               => self::T_COMMAND,
+        
+            self::T_TAG_START       => array(
+                self::T_TAG_NAME,
+                self::T_TAG_CLOSE
             ),
-
-            // tag-inner (inside a tag) state
-            self::T_TAG_INNER => array(
-                '/\{\{(.*?)\}\}/'               => self::T_COMMAND,
-                "/href=\"(javascript:)[^\/]/i"  => self::T_ATTRIBUTE,
-                "/([a-z]+(?:-[a-z]+|))=\"/i"    => self::T_ATTRIBUTE,
-                '/\/?>/'                        => self::T_DATA
+        
+            self::T_TAG_NAME        => array(
+                self::T_TAG_END_OPEN,
+                self::T_TAG_END_CLOSE,
+                self::T_TAG_ATTRIBUTE,
+                self::T_COMMAND
             ),
-            
-            // attribute state
-            self::T_ATTRIBUTE => array(
-                '/\{\{(.*?)\}\}/'               => self::T_COMMAND,
-                "/(?!\\\\)\"/"                  => self::T_TAG
-            ),
-            
-            // javascript state
-            self::T_JAVASCRIPT => array(
-                '/\{\{(.*?)\}\}/'               => self::T_COMMAND,
-                '/<\/script>/i'                 => self::T_DATA
-            ),
-            
-            // css state
-            self::T_CSS => array(
-                '/\{\{(.*?)\}\}/'               => self::T_COMMAND,
-                '/<\/style>/i'                  => self::T_DATA
+        
+            self::T_TAG_ATTRIBUTE   => array(
+                self::T_TAG_END_OPEN,
+                self::T_TAG_END_CLOSE,
+                self::T_TAG_ATTRIBUTE,
+                self::T_COMMAND
             )
         );
         /**/
 
         /**
-         * State to escape mapping.
+         * Names of parser tokens, generated by class constructor.
          *
-         * @octdoc  p:htmlparser/$mapping
-         * @var     array
+         * @octdoc  p:htmlparser/$tokennames
+         * @var     array|null
          */
-        protected static $mapping = array(
-            self::T_ATTRIBUTE  => \org\octris\core\tpl::T_ESC_ATTR,
-            self::T_CSS        => \org\octris\core\tpl::T_ESC_CSS,
-            self::T_DATA       => \org\octris\core\tpl::T_ESC_HTML,
-            self::T_JAVASCRIPT => \org\octris\core\tpl::T_ESC_JS,
-            self::T_TAG        => \org\octris\core\tpl::T_ESC_TAG,
-            self::T_URI        => \org\octris\core\tpl::T_ESC_URI
-        );
+        protected static $tokennames = null;
         /**/
-
+    
         /**
          * Attributes and their relevant context information.
          *
@@ -137,90 +129,270 @@ namespace org\octris\core\tpl\compiler {
         /**/
 
         /**
-         * HTML-Parser implementation.
+         * HTML document to parse.
          *
-         * @octdoc  m:htmlparser/parse
-         * @param 	string 			$tpl 			Template to parse.
-         * @param 	callback 		$cb_compile		Callback to execute snippet compiler.
-         * @param   callback        $cb_error       Callback for handling parse errors.
-         * @parsm
+         * @octdoc  p:htmlparser/$tpl
+         * @var     string
          */
-        public static function parse($tpl, $cb_compile, $cb_error)
+        protected $tpl;
+        /**/
+    
+        /**
+         * Current offset to start parsing from.
+         *
+         * @octdoc  p:htmlparser/$offset
+         * @var     int
+         */
+        protected $offset = 0;
+        /**/
+    
+        /**
+         * Offset to start parsing from in next iteration.
+         *
+         * @octdoc  p:htmlparser/$next_offset
+         * @var     int
+         */
+        protected $next_offset = 0;
+        /**/
+
+        /**
+         * Current state of parser in document.
+         *
+         * @octdoc  p:htmlparser/$state
+         * @var     int
+         */
+        protected $state = self::T_DATA;
+        /**/
+
+        /**
+         * Whether parser is in a valid state. The parser is in a valid state, if the current parser iteration found something to work with.
+         *
+         * @octdoc  p:htmlparser/$valid
+         * @var     bool
+         */
+        protected $valid = false;
+        /**/
+
+        /**
+         * Current parsed content.
+         *
+         * @octdoc  p:htmlparser/$current
+         * @var     array
+         */
+        protected $current = null;
+        /**/
+
+        /**
+         * Stack for escaping modes in html DATA contexts.
+         *
+         * @octdoc  p:htmlparser/$escape_data
+         * @var     array
+         */
+        protected $escape_data = array('html');
+        /**/
+
+        /**
+         * Array for storing normalized template commands.
+         *
+         * @octdoc  p:htmlparser/$commands
+         * @var     array
+         */
+        protected $commands = array();
+        /**/
+
+        /**
+         * Whether debug-mode is enabled.
+         *
+         * @octdoc  p:htmlparser/$debug
+         * @var     bool
+         */
+        protected $debug;
+        /**/
+
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:htmlparser/__construct
+         * @param   string                  $tpl                        HTML document to parse.
+         * @param   bool                    $debug                      Optional parameter to turn on debug-mode.
+         */
+        public function __construct($tpl, $debug = false) 
         /**/
         {
-            $escape = $state  = self::T_DATA;
-            $offset = 0;
-            $len    = strlen($tpl);
-            
-            $getState = function($state) use ($tpl, &$offset) {
-                $match = false;
-                
-                foreach (self::$patterns[$state] as $pattern => $new_state) {
-                    if (preg_match($pattern, $tpl, $m, PREG_OFFSET_CAPTURE, $offset)) {
-                        if ($match === false || $m[0][1] < $match['offset']) {
-                            $match = array(
-                                'offset'     => $m[0][1],
-                                'state'      => $new_state,
-                                'payload'    => (isset($m[1]) ? $m[1][0] : null),
-                                'len'        => strlen($m[0][0])
-                            );
-                        }
-                    }
-                }
-                
-                return $match;
-            };
+            if (is_null(self::$tokennames)) {
+                $class = new \ReflectionClass($this);
+                self::$tokennames = array_flip($class->getConstants());
+            }
+        
+            $this->tpl   = $this->normalize($tpl);
+            $this->debug = $debug;
 
-            while ($offset < $len) {
-                if (($match = $getState($state)) === false) {
-                    break;
-                }
-                
-                $line = substr_count(substr($tpl, 0, $match['offset']), "\n") + 1;
+            print $this->tpl;
+        }
 
-                if ($match['state'] == self::T_COMMAND) {
-                    // template parser
-                    if (!isset(self::$mapping[$escape])) {
-                        // escaping unknown
-                        $cb_error(__FUNCTION__, __LINE__, $line, sprintf('unknown escaping context "%d"', $escape));
+        /** Implementation of methods required for Iterator interface **/
 
-                        $escape = \org\octris\core\tpl::T_ESC_NONE;
-                    }
+        function rewind() {
+            $this->offset = 0;
+        
+            $this->next();
+        }
 
-                    $tpl = substr_replace(
-                        $tpl,
-                        $cb_compiler(trim($match['payload']), $line, $blocks, $escape),
-                        $match['offset'], 
-                        $match['len']
+        function current() {
+            return $this->current;
+        }
+
+        function key() {
+            return $this->offset;
+        }
+
+        /**
+         * This methods parses the template until a template command is reached. The template command is than evailable as iterator item.
+         *
+         * @octdoc  m:htmlparser/next
+         */
+        public function next() 
+        /**/
+        {
+            $current = null;
+        
+            while (($state = $this->getNextState())) {
+                // parsing in progress
+                switch ($state['state']) {
+                case self::T_COMMAND:
+                    $current = array(
+                        'snippet' => $state['payload'],
+                        'escape'  => end($this->escape_data)
                     );
-
-                    if ($state == self::T_TAG) {
-                        // template command was a tag-name
-                        $state = self::T_TAG_INNER;
-                    }
-                } else {
-                    $escape = $state = $match['state'];
-
-                    if ($match['state'] == self::T_ATTRIBUTE)
-                        $payload = strtolower($match['payload']);
-
-                        if ($payload == 'javascript:') {
-                            // (href) tag with javascript executable attribute value
-                            $escape = self::T_JAVASCRIPT;
-                        } elseif (in_array($payload, self::$attributes['js'])) {
-                            // attribute that executes javascript
-                            $escape = self::T_JAVASCRIPT;
-                        } elseif (in_array($payload, self::$attributes['uri'])) {
-                            // attribute that contains an URI
-                            $escape = self::T_URI;
+                    break(2);
+                case self::T_TAG_START:
+                    break;
+                case self::T_TAG_NAME:
+                    if (substr($state['payload'][0][0], 0, 3) == '_c_') {
+                        die('template command not allowed!');
+                    } else {
+                        switch (strtolower($state['payload'][0][0])) {
+                        case 'script':
+                            array_push($this->escape_data, 'js');
+                            break;
+                        case 'style':
+                            array_push($this->escape_data, 'css');
+                            break;
+                        default:
+                            array_push($this->escape_data, 'html');
+                            break;
                         }
                     }
+                    break;
+                case self::T_TAG_END_CLOSE:
+                    array_pop($this->escape_data);
+                    /** FALL THRU **/
+                case self::T_TAG_END_OPEN:
+                    $this->state = self::T_DATA;
+                    continue(2);
+                case self::T_TAG_ATTRIBUTE:
+                    print "\ncall attribute parser\n";
+                    break;
+                case self::T_TAG_CLOSE:
+                    if (count($this->escape_data) == 1) {
+                        if ($this->escape_data[0] != 'html') $this->escape_data[0] = 'html';
+                    }
+            
+                    array_pop($this->escape_data);
+            
+                    $this->state = self::T_DATA;
+                    continue(2);
                 }
-
-                $offset = $match['offset'] + $match['len'];
+            
+                $this->state = $state['state'];
             }
 
+            $this->current = $current;
+            $this->valid   = (is_array($current));
+        }
+
+        function valid() {
+            return $this->valid;
+        }
+    
+        /** Helper methods for parser **/
+    
+        function getNextState() {
+            $this->offset = $this->next_offset;
+
+            if (!isset(self::$rules[$this->state])) {
+                throw new \Exception(sprintf('no rule for state "%s"', self::$tokennames[$this->state]));
+            }
+        
+            $match = false;
+        
+            foreach (self::$rules[$this->state] as $new_state) {
+                $pattern = self::$patterns[$new_state];
+            
+                if (preg_match($pattern, $this->tpl, $m, PREG_OFFSET_CAPTURE, $this->offset)) {
+                    if ($match === false || $m[0][1] < $match['offset']) {
+                        $payload = array();
+                        if (isset($m[1])) $payload[] = $m[1];
+                        if (isset($m[2])) $payload[] = $m[2];
+                    
+                        $match = array(
+                            'offset'    => $m[0][1],
+                            'state'     => $new_state,
+                            'token'     => self::$tokennames[$new_state],
+                            'payload'   => $payload,
+                            'escape'    => null,
+                            'length'    => strlen($m[0][0])
+                        );
+                
+                        if ($this->debug) $match['match'] = $m[0][0];
+                    }
+                }
+            }
+        
+            if ($match !== false) {
+                $this->next_offset = $match['offset'] + $match['length'];
+            }
+        
+            print_r($match);
+        
+            return $match;
+        }
+    
+        /**
+         * Search and replace all template commands and insert them in a dictionary for simpler HTML parsing.
+         *
+         * @octdoc  m:htmlparser/normalize
+         * @param   string                      $tpl                            HTML document to normalize.
+         * @return  string                                                      Normalized HTML document.
+         */
+        protected function normalize($tpl)
+        /**/
+        {
+            $tpl = preg_replace_callback('/\{\{(.*?)\}\}/', function($m) {
+                $id = '_c_' . uniqid() . '_';
+                $this->commands[$id] = $m[1];
+            
+                return $id;
+            }, $tpl);
+        
             return $tpl;
         }
+    }
+}
+
+namespace {
+    $tpl = <<<XML
+<html>
+    <body {{attr()}}="{{value()}}">
+        {{command()}}
+    </body>
+</html>
+XML;
+
+    $p = new \org\octris\core\tpl\compiler\htmlparser($tpl, true);
+    foreach ($p as $s) {
+        print "----\n";
+        print_r($s);
+        print "----\n";
     }
 }
