77d559fc77d5fe9a7e3919caf51e7a08782f9f7a
Harald Lapp <harald.lapp@gmail.com>
2012-03-25 22:04:39 +0200

    initial commit interface for classes implementing currency exchange service

---
+++ libs/type/money/exchange_if.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\type\money {
+    /**
+     * Interface for classes implementing a money exchange service.
+     *
+     * @octdoc      i:cache/storage_if
+     * @copyright   copyright (c) 2012 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    interface exchange_if
+    /**/
+    {
+        /**
+         * Return exchange rate between a source and a target currency.
+         *
+         * @octdoc  m:openexchangerates/getExchangeRate
+         * @param   string              $cur_source             Source currency (ISO 4217).
+         * @param   string              $cur_target             Target currency (ISO 4217).
+         * @return  float                                       Exchange rate.
+         */
+        public function getExchangeRate($cur_source, $cur_target);
+        /**/
+    }
+}
