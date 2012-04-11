96dccf679340b2cf0312839d0a716bc0441b8e75
Harald Lapp <harald.lapp@gmail.com>
2012-04-11 02:14:45 +0200

    initial commit cache storage base class

---
+++ libs/cache/storage.class.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\cache {
+    /**
+     * Cache storage base class.
+     *
+     * @octdoc      c:cache/storage
+     * @copyright   copyright (c) 2012 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    abstract class storage
+    /**/
+    {
+        /**
+         * Storage namespace.
+         *
+         * @octdoc  p:storage/$ns
+         * @var     string
+         */
+        protected $ns = '';
+        /**/
+        
+        /**
+         * Namespace separator.
+         *
+         * @octdoc  p:storage/$ns_separator
+         * @var     string
+         */
+        protected $ns_separator = ':';
+        /**/
+
+        /**
+         * Time to live in seconds.
+         *
+         * @octdoc  p:storage/$ttl
+         * @var     int
+         */
+        protected $ttl = 0;
+        /**/
+
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:storage/__construct
+         */
+        public function __construct(array $options)
+        /**/
+        {
+            if (isset($options['ns_separator'])) {
+                $this->ns_separator = $options['ns_separator'];
+            }
+            if (isset($options['ns'])) {
+                $this->ns = $options['ns'] . $this->ns_separator;
+            }
+            if (isset($options['ttl'])) {
+                $this->ttl = $options['ttl'];
+            }
+        }
+    }
+}
