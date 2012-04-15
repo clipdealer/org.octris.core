22be4f9047d67c2961e97ee1545b0fecf6cb1913
Harald Lapp <harald.lapp@gmail.com>
2012-04-16 00:19:46 +0200

    added test for http client class

---
+++ tests/net/httpTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once('org.octris.core/app/test.class.php');
+
+use \org\octris\core\app\test as test;
+
+class httpTest extends PHPUnit_Framework_TestCase {
+    public function testFetch() {
+        $curl = new \org\octris\core\net\http(new \org\octris\core\type\uri('http://www.example.org/'));
+        $result = $curl->execute();
+
+        print_r($result);
+        die;
+    }
+}
