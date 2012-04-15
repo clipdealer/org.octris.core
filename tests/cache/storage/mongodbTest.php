d15cbc2cacd8714f9478246ffbdf32dd020aec00
Harald Lapp <harald.lapp@gmail.com>
2012-04-15 23:13:24 +0200

    initial commit empty tests for other cache storage adapters

---
+++ tests/cache/storage/mongodbTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(__DIR__ . '/../storageTest.php');
+
+class mongodbTest extends storageTest {
+    public function setUp() {
+        // $this->storage = new \org\octris\core\cache\storage\mongodb(array(
+        //     'ns' => 'org.octris.core.test'
+        // ));
+
+        parent::setUp();
+    }
+}
