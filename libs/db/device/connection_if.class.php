f6c3d97c6a9232991a2619b26018f39f8e6e9d2a
Harald Lapp <harald.lapp@gmail.com>
2012-04-24 23:30:28 +0200

    cleanup

--- libs/db/device/connection_if.class.php
+++ libs/db/device/connection_if.class.php
@@ -9,27 +9,17 @@
  * file that was distributed with this source code.
  */
 
-namespace org\octris\core\db {
+namespace org\octris\core\db\device {
     /**
      * Interface for database connection.
      *
-     * @octdoc      i:db/connection_if
+     * @octdoc      i:device/connection_if
      * @copyright   copyright (c) 2012 by Harald Lapp
      * @author      Harald Lapp <harald@octris.org>
      */
     interface connection_if 
     /**/
     {
-        /**
-         * Constructor.
-         *
-         * @octdoc  m:connection_if/__construct
-         * @param   \org\octris\core\db         $pool               Instance of connection pool.
-         * @param   array                       $options            Connection options.
-         */
-        public function __construct(\org\octris\core\db $pool, array $options);
-        /**/
-
         /**
          * Release connection.
          *
