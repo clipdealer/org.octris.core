23a20e13a059dd3f1c6806e8badcffc415a83f99
Harald Lapp <harald.lapp@gmail.com>
2012-11-28 02:27:16 +0100

    initial commit riak data iterator

---
+++ libs/db/device/riak/dataiterator.class.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\db\device\riak {
+    /**
+     * Riak data object
+     *
+     * @octdoc      c:riak/dataobject
+     * @copyright   copyright (c) 2012 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class dataiterator implements \Iterator
+    /**/
+    {
+        /**
+         * The dataobject to iterate.
+         *
+         * @octdoc  p:dataiterator/$data
+         * @var     \org\octris\core\db\device\riak\subobject
+         */
+        protected $data;
+        /**/
+
+        /**
+         * Keys stored in dataobject.
+         *
+         * @octdoc  p:dataiterator/$keys
+         * @var     array
+         */
+        protected $keys;
+        /**/
+
+        /**
+         * Internal pointer position.
+         *
+         * @octdoc  p:dataiterator/$position
+         * @var     int
+         */
+        protected $position = 0;
+        /**/
+
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:dataiterator/__construct
+         * @parem   \org\octris\core\db\device\riak\subobject    $dataobject         The dataobject to iterate.
+         */
+        public function __construct(\org\octris\core\db\device\riak\subobject $dataobject)
+        /**/
+        {
+            $this->data = $dataobject;          
+            $this->keys = $dataobject->getKeys();
+        }
+
+        /** Iterator **/
+
+        /**
+         * Get value of item.
+         *
+         * @octdoc  m:dataiterator/current
+         * @return  mixed                                                               Value stored at current position.
+         */
+        public function current()
+        /**/
+        {
+            return $this->data[$this->keys[$this->position]];
+        }
+
+        /**
+         * Get key of current item.
+         *
+         * @octdoc  m:dataiterator/key
+         * @return  scalar                                                              Key of current position.
+         */
+        public function key()
+        /**/
+        {
+            return $this->keys[$this->position];
+        }
+
+        /**
+         * Advance pointer.
+         *
+         * @octdoc  m:dataiterator/next
+         */
+        public function next()
+        /**/
+        {
+            ++$this->position;
+        }
+
+        /**
+         * Reset pointer.
+         *
+         * @octdoc  m:dataiterator/rewind
+         */
+        public function rewind()
+        /**/
+        {
+            $this->position = 0;
+        }
+
+        /**
+         * Test if current pointer position is valid.
+         *
+         * @octdoc  m:dataiterator/valid
+         * @return  bool                                                                True, if position is valid.
+         */
+        public function valid()
+        /**/
+        {
+            return isset($this->keys[$this->position]);
+        }
+    }
+}
