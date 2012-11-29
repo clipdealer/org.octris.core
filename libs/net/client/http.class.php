913d71a8eaed378d680c2ff832c79f354964d8b8
Harald Lapp <harald.lapp@gmail.com>
2012-11-29 01:09:22 +0100

    added code for parsing response headers, added method to return response headers of last request

--- libs/net/client/http.class.php
+++ libs/net/client/http.class.php
@@ -54,6 +54,15 @@ namespace org\octris\core\net\client {
         /**/
 
         /**
+         * Stores response headers of last request.
+         *
+         * @octdoc  p:http/$response_headers
+         * @var     array
+         */
+        protected $response_headers = array();
+        /**/
+
+        /**
          * Constructor.
          *
          * @octdoc  m:http/__construct
@@ -113,6 +122,18 @@ namespace org\octris\core\net\client {
         }
 
         /**
+         * Get headers of response.
+         *
+         * @octdoc  m:http/getResponseHeaders
+         * @return  array                                           Response headers.
+         */
+        public function getResponseHeaders()
+        /**/
+        {
+            return $this->response_headers;
+        }
+
+        /**
          * Set maximum redirects for following HTTP location header redirects.
          *
          * @octdoc  m:http/setMaxRedirects
@@ -162,6 +183,63 @@ namespace org\octris\core\net\client {
         /**/
         {
             $this->options[CURLOPT_REFERER] = $referer;
+        }
+
+        /**
+         * Execute client.
+         *
+         * @octdoc  m:http/execute
+         */
+        public function execute()
+        /**/
+        {
+            // setup buffers for storing response data
+            $buf_headers = new \org\octris\core\net\buffer();
+            $this->options[CURLOPT_HEADERFUNCTION] = function($ch, $data) use ($buf_headers) {
+                $data = preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $data);
+                
+                return $buf_headers->write($data);
+            };
+
+            // execute request
+            $return = parent::execute();
+            
+            // process data stored in buffers
+            $this->response_headers = $this->parseResponseHeaders($buf_headers);
+            
+            unset($buf_headers);
+        }
+        
+        /**
+         * Parse response headers.
+         *
+         * @octdoc  p:http/parseResponseHeaders
+         * @param   \org\octris\core\net\buffer             $buffer                 Instance of buffer to parse content of.
+         * @return  array                                                           Contains parsed headers.
+         */
+        protected static function parseResponseHeaders(\org\octris\core\net\buffer $buffer)
+        /**/
+        {
+            $headers = array();
+            
+            foreach ($buffer as $header) {
+                if (preg_match('/^([^:]+?): (.+)/', $header, $match)) {
+                    $name  = strtolower($match[1]);
+                    $value = trim($match[2]);
+                    
+                    if (isset($headers[$name])) {
+                        if (!is_array($headers[$name])) {
+                            $headers[$name] = array($headers[$name]);
+                        }
+                        
+                        $headers[$name][] = $value;
+                    } else {
+                        $headers[$name] = $value;
+                    }
+                }
+            }
+            
+            return $headers;
         }
     }
 }
