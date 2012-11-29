bfb36c0691be181106ac27851bd3412639d6746a
Harald Lapp <harald.lapp@gmail.com>
2012-11-29 01:57:12 +0100

    moved methods over to new collection object

--- libs/db/device/mongodb/connection.class.php
+++ libs/db/device/mongodb/connection.class.php
@@ -51,7 +51,7 @@ namespace org\octris\core\db\device\mongodb {
          * Constructor.
          *
          * @octdoc  m:connection/__construct
-         * @param   \org\octris\core\db\device\mongodb  $device     Device the connection belongs to.
+         * @param   \org\octris\core\db\device\mongodb  $device             Device the connection belongs to.
          * @param   array                               $options            Connection options.
          */
         public function __construct(\org\octris\core\db\device\mongodb $device, array $options)
@@ -107,19 +107,6 @@ namespace org\octris\core\db\device\mongodb {
         }
 
         /**
-         * Create an empty object for storing data into specified collection.
-         *
-         * @octdoc  m:connection/create
-         * @param   string          $collection                         Name of collection to create object for.
-         * @return  \org\octris\core\db\device\mongodb\dataobject       Data object.
-         */
-        public function create($collection)
-        /**/
-        {
-            return new \org\octris\core\db\device\mongodb\dataobject($this->device, $collection);
-        }
-
-        /**
          * Execute a database command.
          *
          * @octdoc  m:connection/command
@@ -134,139 +121,19 @@ namespace org\octris\core\db\device\mongodb {
         }
 
         /**
-         * Query the database and count the results.
+         * Return instance of collection object.
          *
-         * @octdoc  m:connection/count
-         * @param   string          $collection                 Name of collection to query.
-         * @param   array           $query                      Query conditions.
-         * @param   int             $offset                     Optional offset to start query result from.
-         * @param   int             $limit                      Optional limit of result items.
-         * @return  int                                         Number of items found.
+         * @octdoc  m:connection/getCollection
+         * @param   string          $name                               Name of collection to return instance of.
+         * @return  \org\octris\core\db\device\mongodb\collection
          */
-        public function count($collection, array $query, $offset = 0, $limit = null)
+        public function getCollection($name)
         /**/
         {
-            $cl = $this->db->selectCollection($collection);
-
-            return $cl->count($query, $offset, $limit);
-        }
-
-        /**
-         * Create an index in database.
-         *
-         * @octdoc  m:connection/ensureIndex
-         * @param   string          $collection                 Name of collection to create index in.
-         * @param   array           $keys                       Key(s) to create index for.
-         * @param   array           $options                    Optional options for index.
-         */
-        public function ensureIndex($collection, array $keys, array $options = array())
-        /**/
-        {
-            $cl = $this->db->selectCollection($collection);
-
-            $cl->ensureIndex($keys, $options);
-        }
-
-        /**
-         * Query a MongoDB collection and return the first found item.
-         *
-         * @octdoc  m:connection/first
-         * @param   string          $collection                         Name of collection to query.
-         * @param   array           $query                              Query conditions.
-         * @param   array           $sort                               Optional sorting parameters.
-         * @param   array           $fields                             Optional fields to return.
-         * @param   array           $hint                               Optional query hint.
-         * @return  \org\octris\core\db\device\mongodb\dataobject|bool  Either a data object containing the found item or false if no item was found.
-         */
-        public function first($collection, array $query, array $sort = null, array $fields = array(), array $hint = null)
-        /**/
-        {
-            $cursor = $this->query($collection, $query, 0, 1, $sort, $fields, $hint);
-
-            return ($cursor->next() ? $cursor->current : false);
-        }
-
-        /**
-         * Query a MongoDB collection.
-         *
-         * @octdoc  m:connection/query
-         * @param   string          $collection                 Name of collection to query.
-         * @param   array           $query                      Query conditions.
-         * @param   int             $offset                     Optional offset to start query result from.
-         * @param   int             $limit                      Optional limit of result items.
-         * @param   array           $sort                       Optional sorting parameters.
-         * @param   array           $fields                     Optional fields to return.
-         * @param   array           $hint                       Optional query hint.
-         * @return  \org\octris\core\db\device\mongodb\result   Result object.
-         */
-        public function query($collection, array $query, $offset = 0, $limit = null, array $sort = null, array $fields = array(), array $hint = null)
-        /**/
-        {
-            $cl = $this->db->selectCollection($collection);
-
-            if (($cursor = $cl->find($query, $fields)) === false) {
-                throw new \Exception('unable to query database');
-            } else {
-                if (!is_null($sort)) {
-                    $cursor->sort($sort);
-                }
-                if ($offset > 0) {
-                    $cursor->skip($offset);
-                }
-                if (!is_null($limit)) {
-                    $cursor->limit($limit);
-                }
-            }
-            
-            return new \org\octris\core\db\device\mongodb\result($this->device, $collection, $cursor);
-        }
-
-        /**
-         * Insert an object into a database collection.
-         *
-         * @octdoc  m:connection/insert
-         * @param   string          $collection                 Name of collection to insert data into.
-         * @param   array           $object                     Data to insert into collection.
-         */
-        public function insert($collection, array $object)
-        /**/
-        {
-            $cl = $this->db->selectCollection($collection);
-        
-            return $cl->insert($object);
-        }
-
-        /**
-         * Update data in database collection.
-         *
-         * @octdoc  m:connection/update
-         * @param   string          $collection                 Name of collection to update data in.
-         * @param   array           $criteria                   Search criteria for object(s) to update.
-         * @param   array           $object                     Data to update collection with.
-         * @param   array           $options                    Optional options.
-         */
-        public function update($collection, array $criteria, array $object, array $options = null)
-        /**/
-        {
-            $cl = $this->db->selectCollection($collection);
-            
-            return $cl->update($criteria, $object, $options);
-        }
-
-        /**
-         * Remove data from database.
-         *
-         * @octdoc  m:connection/remove
-         * @param   string          $collection                 Name of collection to remove data from.
-         * @param   array           $criteria                   Search criteria for object(s) to remove.
-         * @param   array           $options                    Optional options.
-         */
-        public function remove($collection, array $criteria, array $options = array())
-        /**/
-        {
-            $cl = $this->db->selectCollection($collection);
-            
-            $cl->remove($criteria, $options);
+            return new \org\octris\core\db\device\mongodb\collection(
+                $this->device, 
+                $this->db->selectCollection($name)
+            );
         }
     }
 }
