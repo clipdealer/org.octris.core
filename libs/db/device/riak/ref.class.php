366239f29300b342be46cf5f6be7b4ce9a91016f
Harald Lapp <harald.lapp@gmail.com>
2012-12-13 23:35:12 +0100

    initial commit bucket link references

---
+++ libs/db/device/riak/ref.class.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\core\db\device\riak {
+    /**
+     * Bucket / link reference.
+     *
+     * @octdoc      c:riak/ref
+     * @copyright   copyright (c) 2012 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class ref
+    /**/
+    {
+        /**
+         * Name of bucket to reference to.
+         *
+         * @octdoc  p:ref/$bucket
+         * @var     string
+         */
+        protected $bucket;
+        /**/
+        
+        /**
+         * Key to reference to.
+         *
+         * @octdoc  p:ref/$key
+         * @var     string
+         */
+        protected $key;
+        /**/
+        
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:ref/__construct
+         * @param   string          $bucket             Name of bucket to link to.
+         * @param   string          $key                Key in bucket to link to.
+         */
+        public function __construct($bucket, $key)
+        /**/
+        {
+            $this->bucket = $bucket;
+            $this->key    = $key;
+        }
+    }
+}
