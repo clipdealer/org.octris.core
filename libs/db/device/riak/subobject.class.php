81abd96a6b6b978a0f15265f5d066a254330d035
Harald Lapp <harald.lapp@gmail.com>
2012-12-15 16:36:07 +0100

    code moved to common abstract subobject

--- libs/db/device/riak/subobject.class.php
+++ libs/db/device/riak/subobject.class.php
@@ -17,91 +17,9 @@ namespace org\octris\core\db\device\riak {
      * @copyright   copyright (c) 2012 by Harald Lapp
      * @author      Harald Lapp <harald@octris.org>
      */
-    class subobject implements \ArrayAccess, \Countable, \IteratorAggregate, \JsonSerializable
+    class subobject extends \org\octris\core\db\type\subobject
     /**/
     {
-        /**
-         * Data to store in object.
-         *
-         * @octdoc  p:subobject/$data
-         * @var     array
-         */
-        protected $data = array();
-        /**/
-
-        /**
-         * Constructor.
-         *
-         * @octdoc  m:subobject/__construct
-         * @param   array           $data           Data to initialize object with,
-         */
-        public function __construct(array $data = array())
-        /**/
-        {
-            foreach ($data as $key => $value) {
-                $this[$key] = $value;
-            }
-        }
-
-        /**
-         * Supports deep copy cloning.
-         *
-         * @octdoc  m:subobject/__clone
-         */
-        public function __clone()
-        /**/
-        {
-            foreach ($this->data as $key => $value) {
-                if ($value instanceof self) {
-                    $this[$key] = clone($value);
-                }
-            }
-        }
-
-        /**
-         * Magic method gets called, when 'json_encode' is used on the object instance.
-         *
-         * @octdoc  m:subobject/jsonSerialize
-         * @return  array                                   Array representation of object.
-         */
-        public function jsonSerialize()
-        /**/
-        {
-            return $this->getArrayCopy();
-        }
-
-        /**
-         * Convert to array.
-         *
-         * @octdoc  m:subobject/getArrayCopy
-         * @return  array                                   Array representation of object.
-         */
-        public function getArrayCopy()
-        /**/
-        {
-            $data = $this->data;
-
-            foreach ($data as $key => $value) {
-                if ($value instanceof self) {
-                    $data[$key] = $value->getArrayCopy();
-                }
-            }
-
-            return $data;
-        }
-
-        /**
-         * Return an array of keys stored in object.
-         *
-         * @octdoc  m:subobject/getKeys
-         * @return  array                                   Stored keys.
-         */
-        public function getKeys()
-        /**/
-        {
-            return array_keys($this->data);
-        }
-
         /**
          * Cast values to Riak specific types.
          *
@@ -140,93 +58,17 @@ namespace org\octris\core\db\device\riak {
             return $value;
         }
 
-        /** ArrayAccess **/
-
         /**
-         * Get object property.
+         * Create a new instance of subobject.
          *
-         * @octdoc  m:subobject/offsetGet
-         * @param   string          $name                   Name of property to get.
-         * @return  mixed                                   Data stored in property.
+         * @octdoc  a:subject/createSubObject
+         * @param   array           $data                   Data to create subobject from.
+         * @return  \org\octris\core\db\type\subobject      Created subobject.
          */
-        public function offsetGet($name)
+        public function createSubObject(array $data)
         /**/
         {
-            return $this->castFrom($this->data[$name]);
-        }
-
-        /**
-         * Set object property.
-         *
-         * @octdoc  m:subobject/offsetSet
-         * @param   string          $name                   Name of property to set.
-         * @param   mixed           $value                  Value to set for property.
-         */
-        public function offsetSet($name, $value)
-        /**/
-        {
-            if (is_array($value)) {
-                $value = new self($value);
-            }
-
-            if ($name === null) {
-                $this->data[] = $this->castTo($value);
-            } else {
-                $this->data[$name] = $this->castTo($value);
-            }
-        }
-
-        /**
-         * Unset an object property.
-         *
-         * @octdoc  m:subobject/offsetUnset
-         * @param   string          $name                   Name of property to unset.
-         */
-        public function offsetUnset($name)
-        /**/
-        {
-            unset($this->data[$name]);
-        }
-
-        /**
-         * Test if an object property exists.
-         *
-         * @octdoc  m:subobject/offsetExists
-         * @param   string          $name                   Name of property to test.
-         * @return  bool                                    Returns true if a property exists.
-         */
-        public function offsetExists($name)
-        /**/
-        {
-            return isset($this->data[$name]);
-        }
-
-        /** Countable **/
-
-        /**
-         * Returns number of items stored in object.
-         *
-         * @octdoc  m:subobject/count
-         * @return  int                                     Number of items stored.
-         */
-        public function count()
-        /**/
-        {
-            return count($this->data);
-        }
-
-        /** IteratorAggregate **/
-
-        /**
-         * Return iterator to iterate over object data.
-         *
-         * @octdoc  m:subobject/getIterator
-         * @return  \org\octris\core\db\device\riak\dataiterator        Instance of iterator.
-         */
-        public function getIterator()
-        /**/
-        {
-            return new \org\octris\core\db\device\riak\dataiterator(clone($this));
+            return new self($data);
         }
     }
 }
