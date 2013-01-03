58be14985eb870a1fd856c25ef0ffb4581e9c16b
Harald Lapp <harald.lapp@gmail.com>
2013-01-04 00:12:28 +0100

    initial commit new app starter

---
+++ tools/project/data/skel/cli/run.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the '{{$directory}}' package.
+ *
+ * (c) {{$author}} <{{$email}}>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * {{$description}}
+ *
+ * @octdoc      h:{{$module}}/run.php
+ * @copyright   copyright (c) {{$year}} by {{$company}}
+ * @author      {{$author}} <{{$email}}>
+ */
+/**/
+
+require_once(__DIR__ . '/libs/autoloader.class.php');
+
+$main = new {{$namespace}}\libs\main();
+$main->run();
