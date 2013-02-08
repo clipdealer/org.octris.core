806157e7e999b23dbeed84ebdd208c29bbf949af
Harald Lapp <harald.lapp@gmail.com>
2013-02-08 13:27:33 +0100

    initial commit new skeleton for libraries

---
+++ tools/project/data/skel/lib/libs/$module.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the '{{$directory}}' package.
+ *
+ * (c) {{$author}} <{{$email}}>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace {{$namespace}} {
+    /**
+     * {{$module}}
+     *
+     * @octdoc      c:libs/{{$module}}
+     * @copyright   copyright (c) {{$year}} by {{$company}}
+     * @author      {{$author}} <{{$email}}>
+     */
+    class {{$module}}
+    /**/
+    {
+    }
+}
