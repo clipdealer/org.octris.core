460b4030440ed810afd0a2a1bc999ed31ab2c6fd
Harald Lapp <harald@octris.org>
2013-09-05 21:12:30 +0200

    work on sqlite connection class

--- libs/db/device/sqlite/connection.class.php
+++ libs/db/device/sqlite/connection.class.php
@@ -14,7 +14,7 @@ namespace org\octris\core\db\device\sqlite {
      * SQLite connection handler.
      *
      * @octdoc      c:sqlite/connection
-     * @copyright   copyright (c) 2012 by Harald Lapp
+     * @copyright   copyright (c) 2012-2013 by Harald Lapp
      * @author      Harald Lapp <harald@octris.org>
      */
     class connection extends \SQLite3 implements \org\octris\core\db\device\connection_if
@@ -33,6 +33,74 @@ namespace org\octris\core\db\device\sqlite {
         }
 
         /**
+         * Release connection.
+         *
+         * @octdoc  m:connection/release
+         */
+        public function release()
+        /**/
+        {
+            $this->device->release($this);
+        }
+
+        /**
+         * Check connection.
+         *
+         * @octdoc  m:connection/isAlive
+         * @return  bool                                            Returns true if the connection is alive.
+         */
+        public function isAlive()
+        /**/
+        {
+            return true;
+        }
+
+        /**
+         * Resolve a database reference.
+         *
+         * @octdoc  m:connection_if/resolve
+         * @todo    Implementation.
+         * @param   \org\octris\core\db\type\dbref                          $dbref      Database reference to resolve.
+         * @return  \org\octris\core\db\device\sqlite\dataobject|bool                   Data object or false if reference could not he resolved.
+         */
+        public function resolve(\org\octris\core\db\type\dbref $dbref)
+        /**/
+        {
+            return false;
+        }
+
+        /**
+         * Return list of collections.
+         *
+         * @octdoc  m:connection/getCollections
+         * @todo    Implementation
+         * @return  array|bool                                      Array of names of collections or false in case
+         *                                                          of an error.
+         */
+        public function getCollections()
+        /**/
+        {
+            return array();
+        }
+
+        /**
+         * Return instance of collection object.
+         *
+         * @octdoc  m:connection/getCollection
+         * @param   string          $name                               Name of collection to return instance of.
+         * @return  \org\octris\core\db\device\sqlite\collection        Instance of sqlte collection.
+         */
+        public function getCollection($name)
+        /**/
+        {
+            return new \org\octris\core\db\device\sqlite\collection(
+                $this->device,
+                $this,
+                $name
+            );
+        }
+
+        /**
          * Initialize prepared statement.
          *
          * @octdoc  m:connection/prepare
@@ -43,6 +111,21 @@ namespace org\octris\core\db\device\sqlite {
         /**/
         {
             return new \org\octris\core\db\sqlite\statement($this, $sql);
+        }
+        
+        /**
+         * Execute a SQL query.
+         *
+         * @octdoc  m:connection/query
+         * @param   string                      $sql                SQL statement to execute.
+         * @return  \org\octris\core\db\sqlite\result               Instance of result class.
+         */
+        public function query($sql)
+        /**/
+        {
+            $result = parent::query($sql);
+            
+            return new \org\octris\core\db\sqlite\result($this, $result);
         }
     }
 }
